c1b0370c4ce41e528ffdb2c7e61fb634a8d2378f
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2011-03-16 20:18:49 +0000

    Fixing phase fraction

--- pwncatalog_1yr/analyze.py
+++ pwncatalog_1yr/analyze.py
@@ -4,7 +4,7 @@ from os.path import expandvars as e
 from argparse import ArgumentParser
 
 from uw.like.pointspec import SpectralAnalysis
-from uw.like.pointspec_helpers import get_default_diffuse, PointSource
+from uw.like.pointspec_helpers import get_default_diffuse, PointSource, FermiCatalog
 from uw.like.SpatialModels import Disk
 from uw.like.Models import PowerLaw
 from skymaps import SkyDir
@@ -20,24 +20,21 @@ args=parser.parse_args()
 class Empty(): pass
 
 args=Empty()
-args.all_sources="pwnlist.yaml"
+args.all_sources=e("$PWNSCRIPTS/pwnlist.yaml")
 args.name="PSRJ0835-4510"
 
 name=args.name
 
 sources=yaml.load(open(args.all_sources))
 
-catalog_name=name.replace("PSRJ","PSR J")
+catalog_name=sources[name]['catalog']
 phase=sources[name]['phase']
 ft1=sources[name]['ft1']
 ltcube=sources[name]['ltcube']
 
-phase_fraction=phase[1]-phase[0]
+phase_fraction=phase[1]-phase[0] if phase[1]>phase[0] else (1-phase[1]) + (phase[0]-0)
 
-
-from lande_roi import Catalog2FGL
-catalog=Catalog2FGL(e("$FERMI/catalogs/24M7_uw82.fits"),
-                    e("$FERMI/extended_archives/Extended_archive_v08/"))
+catalog=FermiCatalog(e("$FERMI/catalogs/gll_psc_v02.fit"))
 catalog_source=[i for i in catalog.get_sources(SkyDir(),180) if i.name==catalog_name][0]
 
 center=catalog_source.skydir
@@ -52,7 +49,7 @@ spectral_analysis = SpectralAnalysis(data_specification,
                                      binsperdec = 4,
                                      emin       = 100,
                                      emax       = 100000,
-                                     irf        = "P7SOURCE_V6",
+                                     irf        = "P6_V3_DIFFUSE",
                                      roi_dir    = center,
                                      maxROI     = 10,
                                      minROI     = 10)
@@ -60,17 +57,14 @@ spectral_analysis = SpectralAnalysis(data_specification,
 roi=spectral_analysis.roi(
     roi_dir=center,
     diffuse_sources=get_default_diffuse(diffdir=e("$FERMI/diffuse"),
-        gfile="ring_24month_P76_v1.fits",
-        ifile="isotrop_21month_P76_source_v2.txt"),
+        gfile="gll_iem_v02.fit",
+        ifile="isotropic_iem_v02.txt"),
     catalogs = catalog,
     fit_emin = 100,
     fit_emax = 100000
 )
 
 roi.del_source(catalog_name)
-
-if "Vela X" in roi.dsm.names: roi.del_source("Vela X")
-if "MSH 15-52" in roi.dsm.names: roi.del_source("MSH 15-52")
 
 print roi
 
