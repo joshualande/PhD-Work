17cd1d63a3365d8a89f075c578c93a5ea3b0219b
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2011-03-16 21:34:45 +0000

    Updating the scripts

--- pwncatalog_1yr/setup_pwn.py
+++ pwncatalog_1yr/setup_pwn.py
@@ -1,40 +1,23 @@
 #!/usr/bin/env python
+import yaml
+from os.path import expandvars as e
 
-def setup():
-    import yaml
-    from os.path import expandvars as e
-    from argparse import ArgumentParser
+from uw.like.pointspec import SpectralAnalysis
+from uw.like.pointspec_helpers import get_default_diffuse, PointSource, FermiCatalog
+from uw.like.SpatialModels import Disk
+from uw.like.Models import PowerLaw
+from skymaps import SkyDir
 
-    from uw.like.pointspec import SpectralAnalysis
-    from uw.like.pointspec_helpers import get_default_diffuse, PointSource, FermiCatalog
-    from uw.like.SpatialModels import Disk
-    from uw.like.Models import PowerLaw
-    from skymaps import SkyDir
+def setup_pwn(name,list):
 
-    # Note that ArgumentParser correctly interprets negative numbers as positional arguments!
-    """
-    parser = ArgumentParser()
-    parser.add_argument("-a", "--all_sources", required=True, help="List of all yaml sources")
-    parser.add_argument("-n", "--name", required=True, help="Name of the pulsar")
-    args=parser.parse_args()
-    """
-
-    class Empty(): pass
-
-    args=Empty()
-    args.all_sources=e("$PWNSCRIPTS/pwnlist.yaml")
-    args.name="PSRJ0835-4510"
-
-    name=args.name
-
-    sources=yaml.load(open(args.all_sources))
+    sources=yaml.load(open(list))
 
     catalog_name=sources[name]['catalog']
     phase=sources[name]['phase']
     ft1=sources[name]['ft1']
     ltcube=sources[name]['ltcube']
 
-    phase_fraction=phase[1]-phase[0] if phase[1]>phase[0] else (1-phase[1]) + (phase[0]-0)
+    phase_factor=phase[1]-phase[0] if phase[1]>phase[0] else (1-phase[1]) + (phase[0]-0)
 
     catalog=FermiCatalog(e("$FERMI/catalogs/gll_psc_v02.fit"))
     catalog_source=[i for i in catalog.get_sources(SkyDir(),180) if i.name==catalog_name][0]
@@ -63,16 +46,11 @@ def setup():
             ifile="isotropic_iem_v02.txt"),
         catalogs = catalog,
         fit_emin = 100,
-        fit_emax = 100000
+        fit_emax = 100000,
+        phase_factor = phase_factor
     )
 
     roi.del_source(catalog_name)
-
-    print roi
-
-    roi.fit()
-
-    print roi
 
     # make residual TS map
 
