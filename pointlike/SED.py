6841b25882cf3f47fd47502e955bf31e223c5e8d
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2011-07-28 21:19:57 +0000

    rename file

--- pointlike/SED.py
+++ pointlike/SED.py
@@ -1,5 +1,9 @@
 """
-Implements a Module to calculate SEDs.
+Implements a Module to calculate SEDs using the pyLikelihood framework.
+
+The primary benefit to this implementation is that it requires no
+temporary files. All you need is a BinnedAnalysis/UnbinnedAnalysis and
+you can directly generate the SED.
 
 Usage:
 
@@ -19,26 +23,39 @@ import math
 import numpy as np
 
 from LikelihoodState import LikelihoodState
-import IntegralUpperLimit
+from pyLikelihood import dArg
 
 class SED(object):
     """ object to make SEDs using pyLikelihood. """
 
+    ul_choices = ['frequentist', 'bayesian']
+
     def __init__(self, like, name, verbosity=0, 
                  freeze_background=True,
-                 always_upper_limit=False):
+                 always_upper_limit=False,
+                 ul_algorithm='bayesian',
+                 powerlaw_index=-2):
         """ Parameters:
             * like - pyLikelihood object
             * name - source to make an SED for
             * verbosity - how much output
             * freeze_background - don't refit background sources.
             * always_upper_limit - Always compute an upper limit. Default 
-                                   is only when source is not significant. """
+                                   is only when source is not significant. 
+            * ul_algorithm - choices = 'frequentist', 'bayesian' 
+            * powerlaw_index - fixed spectral index to assume when
+                               computing SED.
+        """
         self.like               = like
         self.name               = name
         self.verbosity          = verbosity
         self.freeze_background  = freeze_background
         self.always_upper_limit = always_upper_limit
+        self.ul_algorithm       = ul_algorithm
+        self.powerlaw_index     = powerlaw_index
+
+        if ul_algorithm not in self.ul_choices:
+            raise Exception("Upper Limit Algorithm %s not in %s" % (ul_algorithm,str(self.ul_choices)))
 
         self.bin_edges = like.energies
         self.e_vals = like.e_vals
@@ -48,16 +65,26 @@ class SED(object):
         self.dnde_err=np.empty_like(self.e_vals)
         self.ts=np.empty_like(self.e_vals)
         self.ul=-1*np.ones_like(self.e_vals) # -1 is no UL
-        self.calculate()
+        self._calculate()
 
     @staticmethod
-    def upper_limit(like,name,verbosity):
-        """ Abstract the way upper limits are calculated.
-            For now, use Bayesian method. Could be
-            generalized... 
-            
-            Do the integral approximatly: don't fit background 
-            parameters during fit. """
+    def frequentist_upper_limit(like,name,verbosity,emin,emax):
+        """ Calculate a frequentist upper limit on the
+            prefactor. """
+        import UpperLimits
+        ul = UpperLimits.UpperLimits(like)
+        ul_prof, par_prof = ul[name].compute(emin=emin, emax=emax, verbosity=verbosity)
+        prefactor=like[like.par_index(name, 'Prefactor')]
+        return par_prof*prefactor.getScale()
+
+    @staticmethod
+    def bayesian_upper_limit(like,name,verbosity):
+        """ Calculate a baysian upper limit.
+            Return the fit prefactor value (which is dN/dE
+            in the geometric mean of the energy bin
+            since scale is set to the geometric mean
+            of the energy bin). """
+        import IntegralUpperLimit
         ul_flux,results = IntegralUpperLimit.calc_int(like, name, 
                                                       freeze_all=True,
                                                       skip_global_opt=True,
@@ -65,7 +92,7 @@ class SED(object):
                                                       verbosity=verbosity)
         return results['xlim']
 
-    def calculate(self):
+    def _calculate(self):
         """ Compute the flux data points for each energy. """
 
         like    = self.like
@@ -88,7 +115,7 @@ class SED(object):
             for i in range(len(like.model.params)):
                 like.freeze(i)
 
-        # convert source to a PowerLaw of (frozen) index 2
+        # convert source to a PowerLaw of frozen index 
 
         source = like.logLike.getSource(name)
         old_spectrum=source.spectrum()
@@ -98,7 +125,7 @@ class SED(object):
         prefactor=like[like.par_index(name, 'Prefactor')]
         scale=like[like.par_index(name, 'Scale')]
 
-        index.setValue(-2/index.getScale())
+        index.setValue(self.powerlaw_index/index.getScale())
         index.setFree(0)
         prefactor.setScale(1e-11)
         prefactor.setBounds(1e-10,1e10)
@@ -119,12 +146,15 @@ class SED(object):
 
             like.setEnergyRange(float(lower)+1, float(upper)-1)
 
-            like.fit(covar=True,optverbosity)
+            like.fit(optverbosity,covar=True)
             self.ts[i]=like.Ts(name,reoptimize=False)
 
             if self.ts[i] < 9 or self.always_upper_limit: 
                 if verbosity: print 'Calculating upper limit from %.0dMeV to %.0dMeV' % (lower,upper)
-                self.ul[i] = SED.upper_limit(like,name,verbosity)
+                if self.ul_algorithm == 'frequentist':
+                    self.ul[i] = SED.frequentist_upper_limit(like,name,verbosity,lower,upper)
+                elif self.ul_algorithm == 'bayesian':
+                    self.ul[i] = SED.bayesian_upper_limit(like,name,verbosity)
 
             prefactor=like[like.par_index(name, 'Prefactor')]
             self.dnde[i] = prefactor.getValue()*prefactor.getScale()
@@ -132,20 +162,24 @@ class SED(object):
             if verbosity:
                 print lower,upper,self.dnde[i],self.dnde_err[i],self.ts[i] ,self.ul[i]
 
-        # revert to old source model
+        # revert to old model
         like.setEnergyRange(self.bin_edges[0],self.bin_edges[-1])
         like.setSpectrum(name,old_spectrum)
         saved_state.restore()
 
     def __str__(self,terse=True,precision=1):
-        """ Pack up the values into a nicely formatted string.
-            If terse, only include energy, flux, flux_err where flux is
-            replaced by the upper limit if there is only 3 sigma source
-            detection for a given energy. """
+        """ Pack up values into a nicely formatted string.
+
+            If terse=True, only include energy, flux, and flux_err 
+            where flux is replaced by the upper limit if there is 
+            a < 3sigma detection. If terse=False, also include
+            a column of TS values, upper limits, Lower and Upper
+            energies, and fluxes + flux errors (even when
+            insignificant. """
 
         # convert list to scientific notation
         conv_science = lambda vals: ['%.*e' % (precision,i) \
-                                 if isinstance(i,str) else i \
+                                 if not isinstance(i,str) else i \
                                  for i in vals]
         conv_float   = lambda vals: ['%.*f' % (precision,i) for i in vals]
 
@@ -153,12 +187,12 @@ class SED(object):
         sed_vals.append(['Energy', '[MeV]'] + conv_float(self.e_vals))
 
         # contains flux + UL if TS < 9
-        sed_vals.append(['Flux', '[ph/MeV/cm^2/s]'] + \
-                        conv_science([f if ts>=9 else '>'+'%.*e' % (precision,ul) \
+        sed_vals.append(['Flux', '[ph/cm^2/s/MeV]'] + \
+                        conv_science([f if ts>=9 else '>%.*e' % (precision,ul) \
                                   for f,ul,ts in zip(self.dnde,self.ul,self.ts)]))
 
         # contains flux error if TS > 9
-        sed_vals.append(['Flux_Err', '[ph/MeV/cm^2/s]'] + \
+        sed_vals.append(['Flux_Err', ''] + \
                         conv_science([ferr if ts>9 else '' \
                                   for ferr,ts in zip(self.dnde_err,self.ts)]))
 
@@ -179,25 +213,32 @@ class SED(object):
             ''.join(['%15s' % j for j in i]) for i in sed_transpose
         ])
 
-    def save(self,file,precision=5,terse=False,**kwargs):
+    def save(self,filename,precision=5,**kwargs):
+        """ Save SED data points to a file.
+            By default, save with 5 points of
+            precision and save out everything that
+            can be saved out. """
         output=self.__str__(precision=precision,**kwargs)
-        if hasattr(file,'write'):
-            file.write()
+        if hasattr(filename,'write'):
+            filename.write()
         else:
-            f=open(file,'w')
+            f=open(filename,'w')
             f.write(output)
             f.close()
 
     def plot(self,filename,plot_spectral_fit=True):
-        import pylab as P
+        try:
+            import pylab as P
+        except:
+            raise Exception("The plot function requires pylab.")
 
         P.plot(self.e_vals,self.e_vals**2*self.dnde)
 
         if plot_spectral_fit:
-            source = like.logLike.getSource(self.name)
+            source = self.like.logLike.getSource(self.name)
             spectrum=source.spectrum()
             elist = np.logspace(self.like.energies[0],self.like.energies[-1])
-            flist = spectrum(elist)
+            flist = np.asarray([spectrum(dArt(i)) for i in elist])
             P.plot(elist,elist**2*flist)
 
         P.set_xscale('log')
