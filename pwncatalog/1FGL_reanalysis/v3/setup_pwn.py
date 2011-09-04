d29e7b8c13932acceb221e960026feae30f35a2e
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2011-09-04 21:53:10 +0000

    adding phasing to setup_pwn

--- pwncatalog/1FGL_reanalysis/v3/setup_pwn.py
+++ pwncatalog/1FGL_reanalysis/v3/setup_pwn.py
@@ -1,38 +1,39 @@
 #!/usr/bin/env python
 import yaml
-from os.path import expandvars as e
+from os.path import expandvars as e, join as j
+
+from tempfile import mkdtemp
 
 from uw.like.pointspec import SpectralAnalysis
 from uw.like.pointspec_helpers import get_default_diffuse, PointSource, FermiCatalog
 from uw.like.SpatialModels import Disk
 from uw.like.Models import PowerLaw
 from skymaps import SkyDir
+from uw.utilities import phasetools
 
-def setup_pwn(name,pwnlist,phasing=True):
+import toolkit
+
+def setup_pwn(name,pwndata,phase_ranges,tempdir=None):
     """Name of the source
-    pwnlist Yaml file
-    phasing=true : apply phase cut
-    phasing=false : don't do it
+    pwndata Yaml file
     
     returns pointlike ROI.
     """
 
-    sources=yaml.load(open(pwnlist))
+    sources=yaml.load(open(pwndata))
 
     catalog_name=sources[name]['catalog']
-    phase=sources[name]['phase']
     ltcube=sources[name]['ltcube']
-    binfile=sources[name]['binfile']
     pulsar_position=SkyDir(*sources[name]['dir'])
-
     ft2=sources[name]['ft2']
+    ft1=sources[name]['ft1']
 
-    if phasing==True:
-        phase_factor=phase[1]-phase[0] if phase[1]>phase[0] else (1-phase[0]) + (phase[1]-0)
-        ft1=sources[name]['ft1']
-    else :
-        phase_factor=1.0
-        raise Exception("Unable to phase data")
+    # in case no list was passed
+    if len(phase_ranges)==2 and isinstance(phase_ranges[0],numbers.Real) and \
+       isinstance(phase_ranges[1],numbers.Real):
+        phase_ranges = [phase_ranges] 
+
+    phase_factor=toolkit.phase_ranges(phase_ranges)
 
 
     catalog=FermiCatalog(e("$FERMI/catalogs/gll_psc_v02.fit"),free_radius=5)
@@ -40,11 +41,23 @@ def setup_pwn(name,pwnlist,phasing=True):
 
     center=catalog_source.skydir
 
+    if tempdir is None: tempdir=mkdtemp()
+
+    binfile=j(tempdir,'binned_phased.fits)
+
+    # apply phase cut to ft1 file
+    phased_ft1 = j(tempdir,'ft1_phased.fits')
+    phasetools.phase_cut(ft1,phased_ft1,phaseranges=phase_ranges)
+
+    # create a temporary ltcube scaled by the phase factor
+    phased_ltcube=j(tempdir,'phased_ltcube.fits')
+    toolkit.phase_ltcube(ltcube,phased_ltcube, phase_ranges=phase_ranges)
+
     from uw.like.pointspec import DataSpecification
     data_specification = DataSpecification(
-                         ft1files = ft1,
+                         ft1files = phased_ft1,
                          ft2files = ft2,
-                         ltcube   = ltcube,
+                         ltcube   = phased_ltcube,
                          binfile  = binfile)
 
     spectral_analysis = SpectralAnalysis(data_specification,
@@ -64,7 +77,7 @@ def setup_pwn(name,pwnlist,phasing=True):
         catalogs = catalog,
         fit_emin = 100,
         fit_emax = 100000,
-        phase_factor = phase_factor)
+        phase_factor = 1) # phaseing already done to the ltcube
 
     # delete original pulsar
     roi.del_source(catalog_name)
