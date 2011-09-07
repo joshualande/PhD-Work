f05509aa6b34705b13c24b80596bc831f6bc0d53
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2011-09-07 22:55:31 +0000

    better loop

--- pwncatalog/1FGL_reanalysis/v3/loop_pwn.py
+++ pwncatalog/1FGL_reanalysis/v3/loop_pwn.py
@@ -1,3 +1,13 @@
+"""
+A script to loop over the PWN when
+doing analysis. Example:
+
+python loop_pwn.py -c analyze_v1.py \
+        --pwndata /u/gl/lande/svn/trunk/pwncatalog/1FGL_reanalysis/v3/pwndata_v1.yaml  \
+        --pwnphase /u/gl/lande/svn/trunk/pwncatalog/1FGL_reanalysis/v3/pwnphase_v1.yaml \
+        -o /nfs/slac/g/ki/ki03/lande/pwncatalog/1FGL_reanalysis/v3/v1/
+
+"""
 import yaml
 from os.path import expandvars as e
 from os.path import join
@@ -41,19 +51,26 @@ temp=open(submit_all,'w')
 temp.write("""
 #!/usr/bin/env python
 import glob
-from os.path import exists,isdir
+from os.path import exists,isdir,expandvars
 from os import system
 import argparse
+import subprocess
 
 parser=argparse.ArgumentParser()
 parser.add_argument("-n",default=False,action="store_true",help="Don't do anything")
 args = parser.parse_args()
 
+p=subprocess.Popen(['bjobs', '-w'],stdout=subprocess.PIPE)
+queue_jobs,err=p.communicate()
+
 for name in glob.iglob("*"):
     if isdir(name):
         results="%s/results_%s.yaml" % (name,name)
         log="%s/log_%s.txt" % (name,name)
-        if not exists(results) and (not exists(log) or "Results reported at" in open(log).read()):
+        run='$PWD/%s/run_%s.sh' % (name,name)
+
+        is_in_queue = expandvars(run) in queue_jobs
+        if not is_in_queue or not exists(results):
             string="cd %s; bsub -q kipac-ibq -oo log_%s.txt sh $PWD/run_%s.sh; cd .." % (name,name,name)
             if args.n:
                 print string
