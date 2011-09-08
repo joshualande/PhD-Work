b77ebb3150802c7397dab8de46823ed5052b9760
rousseau <rousseau@37a9682d-6443-41a2-8582-b44379b6e86f>
2011-09-08 18:48:10 +0000

    add all in a same code

--- pwncatalog/1FGL_reanalysis/v3/pulse_find.py
+++ pwncatalog/1FGL_reanalysis/v3/pulse_find.py
@@ -8,163 +8,150 @@ import numpy as np
 from scipy.optimize import leastsq
 import pylab as P
 
-#parser = ArgumentParser()
-#parser.add_argument("--pwndata", required=True)
-#parser.add_argument("-n", "--name", required=True, help="Name of the pulsar")
-#parser.add_argument("--phcent", type=float, required=True)
-#args=parser.parse_args()
-#
+parser = ArgumentParser()
+parser.add_argument("--pwndata", required=True)
+parser.add_argument("-p", "--pwnphase", required=True)
+parser.add_argument("-n", "--name", required=True, help="Name of the pulsar")
+args=parser.parse_args()
+
 #name=args.name
-#phi_center=args.phcent
-#pwndata=args.pwndata
+name="PSRJ0007+7303"
 
-pwndata='pwndata_v1.yaml'
 
-""" This min and max do not correspond to
-the off pulse phase min and max, but
-to the phase interval over which to
-search for the off pulse phase min and max.
-They correspond to the peak in the phaseogram
-of the two pulsar peaks inside of which
-we look for the off pulse emission. """
+phase=yaml.load(open(args.pwnphase))[name]['phase']
+# guess at center of off pulse region
+phi_center=(phase[0] + ((phase[1]-phase[0]) % 1)/2.0) % 1
 
-#name='PSRJ0534+2200'
-#phase_max=1
-#phase_min=0.3
+def compute_curve(phi_center,npts=50):
+    """Function to compute the points TS vs dphi"""
+    #npts=50
 
-name='PSRJ0007+7303'
-phase_min=0.45-0.35
-phase_max=0.45+0.25
 
-dphimax=(phase_max-phase_min) % 1
-phi_center=(phase_min + dphimax/2) % 1
-#npts=100
-npts=30
-def phasebinanalysis(phi_center_f):
-    #npts=100
-    #npts=10
-    
-    TS=np.zeros(npts)
-    phimin=np.zeros(npts)
-    phimax=np.zeros(npts)
-    
     # don't go all the way to 0 = no photons!
-    dphi = np.linspace(dphimax/10,dphimax,npts)
-    phimin = (phi_center_f - dphi/2) % 1
-    phimax = (phi_center_f + dphi/2) % 1
+    dphi = np.linspace(0,1,npts)[1:]
+    phimin = (phi_center - dphi/2) % 1
+    phimax = (phi_center + dphi/2) % 1
+
+    TS=np.empty_like(dphi)
 
     for i,phase in enumerate(zip(phimin,phimax)):
         print 'phase min=%.2f, max=%.2f (%d/%d)' % (phase[0],phase[1],i+1,npts)
-        
-        roi=setup_pwn(name,pwndata,phase, quiet=True)
+        if phase[0]>phase[1]:
+            phase2=[[phase[0],1.0],[0.0,phase[1]]]
+        else :
+            phase2=phase
+
+        roi=setup_pwn(name,args.pwndata,phase2, quiet=True)
         print 'bin edges:',roi.bin_edges
-        roi.fit(use_gradient=True)
+        roi.fit(method="minuit",use_gradient=True, estimate_errors=False)
         TS[i]=roi.TS(which=name, quick=False)
-
-    middle_index=int(len(dphi)/2.0)
-    dphi_break_0=dphi[middle_index]
-    pwn_0=TS[middle_index]/dphi_break_0
-    psr_0=(TS[-1]-TS[middle_index])/(dphi[-1]-dphi[middle_index])
-    p0=np.asarray([pwn_0,psr_0,dphi_break_0])
-
-    # this function returns TS as a function of dphi
-    def model_ts(p,dphi):
-        # pwn = slope of pwn
-        # psr = slope of psr
-        # dphi_break is break between pwn and pulsar
-        pwn,psr,dphi_break=p
-        return np.where(dphi<dphi_break,
-                        pwn*dphi,
-                        psr*(dphi-dphi_break) + pwn*dphi_break)
-    def model_ts_pl(p,dphi):
-        # pwn = slope of pwn
-        pwn=p
-        return pwn*dphi
-    errfunc = lambda p, dphi: TS-model_ts(p,dphi)
-    p1, success = leastsq(errfunc,p0,args=(dphi,))
-
-    pwn_1,psr_1,dphi_break_1= p1
-    phimin_1 = (phi_center_f - dphi_break_1/2) % 1
-    phimax_1 = (phi_center_f + dphi_break_1/2) % 1
-    print 'The best dphi is = %.2f. Range = [%.2f, %.2f]' % (dphi_break_1,phimin_1,phimax_1)
-                    
-
-
     
-    p2=pwn_0
 
+        f=open("results_%s_%.2f.yaml" %(name,phi_center)"w")
+        yaml.dump(dict(TS=TS.tolist(), 
+                       dphi=dphi.tolist(),
+                       phi_center=phi_center,
+                       phimin=phimin.tolist(), 
+                       phimax=phimax.tolist()),
+                  f) # todo, add phimin, phimax to file
+        f.close()
+
+        return TS,dphi
+
+def model_ts_line(p,dphi):
+    """Model of a line"""
+    # pwn = slope of pwn
+    pwn=p
+    return pwn*dphi
+
+def find_dec(TS,dphi):
+    """function to find the best phase interval"""
+    p2=pwn_0
     TS2=[]
     TS2.append(TS[0])
     dphi2=[]
+    error=[]
     dphi2.append(dphi[0])
-    for i in range(len(TS)-2):
+    i=0
+    error[0]=0
+    error2=0
+    ind=0
+    while i<(len(TS)-2) and error2<9:
         TS2.append(TS[i+1])
         dphi2.append(dphi[i+1])
-        errfunc2= lambda p2, dphi:TS2-model_ts_pl(p2,dphi2)
+        errfunc2= lambda p2, dphi:TS2-model_ts_pl(p2,dphi)
         p3, success = leastsq(errfunc2,p2,args=(dphi2,))
-        error=TS[i+2]-model_ts_pl(p3,dphi[i+2])
-        if error > 4:
+        error2=TS[i+2]-model_ts_line(p3,dphi[i+2])
+        error.append(error2)
+        print 'error=%.2f'%error2
+        if error2 > 9:
             dphi_break_1=dphi[i+1]
             phimin_1=(phi_center_f -dphi_break_1/2) % 1
             phimax_1 = (phi_center_f + dphi_break_1/2) % 1
-            print 'The best dphi is = %.2f. Range = [%.2f, %.2f]' % (dphi_break_1,phimin_1,phimax_1)
-
-    # plot the results
-
-    f=open("phase_analysis_%s.yaml" % name,"w")
-    yaml.dump(dict(TS=TS.tolist(), dphi=dphi.tolist(),
-                   phimin=phimin.tolist(), phimax=phimax.tolist(),
-                   p0=p0.tolist(),p1=p1.tolist()),f) # todo, add phimin, phimax to file
-    f.close()
-    
-    #plotting code
-    P.figure(1,figsize=(6,6));P.clf()
-    
-    P.ylabel(r'TS')
-    P.xlabel(r'$\Delta\phi$')
-    P.title("TS $(\Delta\phi)$")
-    P.grid(True)
-
-    dphi_model=np.linspace(0,dphi[-1],100)
-    P.plot(dphi_model,model_ts(p1,dphi=dphi_model),label='model')
-    P.plot(dphi,TS, 'o', label='data')
-
-    P.savefig("TS_dphi_%s_%s.png"%(name,phi_center_f))
-
-    return dphi_break_1,phimin_1,phimax_1
-
-
-#Loop on the center position the idea is the following : look to 1 bin in each side of the center. redefine the center as the position fo which the interval is the largest then iterate in the same sens.
-dphicent,phimincent,phimaxcent=phasebinanalysis(phi_center)
-dphilow,phiminlow,phimaxlow=phasebinanalysis(phi_center-dphimax/npts)
-dphihigh,phiminhigh,phimaxhigh=phasebinanalysis(phi_center+dphimax/npts)
-
-f2=open("control.txt","w")
-
-f2.write("dphicent,phimincent,phimaxcent=%.2f\t%.2f\t%.2f\n"%(dphicent,phimincent,phimaxcent))
-f2.write("dphilow,phiminlow,phimaxlow=%.2f\t%.2f\t%.2f\n"%(dphilow,phiminlow,phimaxlow))
-f2.write("dphihigh,phiminhigh,phimaxhigh=%.2f\t%.2f\t%.2f\n"%(dphihigh,phiminhigh,phimaxhigh))
-delta=0
-dphiloop=0
-if dphilow>=dphicent :
-    delta=-dphimax/npts
-    dphiloop=dphilow
-elif dphihigh>=dphicent :
-    delta=+dphimax/npts
-    dphiloop=dphihigh
-else :
-    delta=0.0
-
-f2.write("delta=%.2f\n"%(delta))
-
-if delta !=0.0:
-    i=0
-    while dphiloop<dphicent and i<10:
-        dphiloop=dphicent
-        phi_center+=delta
-        dphicent,phimincent,phimaxcent=phasebinanalysis(phi_center)
+            #print 'The best dphi is = %.2f. Range = [%.2f, %.2f]\t error=%.2f' % (dphi_break_1,phimin_1,phimax_1,error2)
+            ind=i
         i+=1
-        f2.write("dphicent,phimincent,phimaxcent=%.2f\t%.2f\t%.2f\n"%(dphicent,phimincent,phimaxcent))
+    
+    while i<range(len(dphi2)) and error[i]>4:
+        i-=1
+        ind-=1
 
-print "Best fit position :\n\tcenter=%.2f\n\twidth=%.2f\n\tmin=%.2f\n\tmax=%.2f\n"%(dphicent,phimaxcent-phimincent,phimincent,phimaxcent)
-f2.write("Best fit position :\n\tcenter=%.2f\n\twidth=%.2f\n\tmin=%.2f\n\tmax=%.2f\n"%(dphicent,phimaxcent-phimincent,phimincent,phimaxcent))        
+    dphi_break_1=dphi[ind]
+    phimin_1=(phi_center_f -dphi_break_1/2) % 1
+    phimax_1 = (phi_center_f + dphi_break_1/2) % 1
+    print 'The best dphi is = %.2f. Range = [%.2f, %.2f]\t error=%.2f' % (dphi_break_1,phimin_1,phimax_1,error[ind])
+    
+    return dphi_break_1,phimin_1,phimax_1,error
+
+
+def find_center(center,npts):
+   """Loop on the center position the idea is the following : look to 1 bin in each side of the center. redefine the center as the position fo which the interval is the largest then iterate in the same sens."""
+   TS1,dphi1=compute_curve(center,npts=50)
+   dphicent,phimincent,phimaxcent,errorcent=find_dec(TS1,dphi1)
+   #center-1.0/npts
+   TS2,dphi2=compute_curve(center-1.0/npts,npts=50)
+   dphilow,phiminlow,phimaxlow,errorlow=find_dec(TS2,dphi2)
+   #center+1.0/npts
+   TS3,dphi3=compute_curve(center+1.0/npts,npts=50)
+   dphihigh,phiminhigh,phimaxhigh,errorhigh=find_dec(TS3,dphi)
+
+   f2=open("control.txt","w")
+
+   f2.write("dphicent,phimincent,phimaxcent=%.2f\t%.2f\t%.2f\n"%(dphicent,phimincent,phimaxcent))
+   f2.write("dphilow,phiminlow,phimaxlow=%.2f\t%.2f\t%.2f\n"%(dphilow,phiminlow,phimaxlow))
+   f2.write("dphihigh,phiminhigh,phimaxhigh=%.2f\t%.2f\t%.2f\n"%(dphihigh,phiminhigh,phimaxhigh))
+   delta=0
+   dphiloop=0
+   if dphilow>=dphicent :
+       delta=-1.0/npts
+       dphiloop=dphilow
+       dphicent=dphiloop+1
+   elif dphihigh>=dphicent :
+       delta=+1.0/npts
+       dphiloop=dphihigh
+       dphicent=dphiloop+1
+   else :
+       delta=0.0
+
+   f2.write("delta=%.2f\n"%(delta))
+       
+   if delta !=0.0:
+       i=0
+       while dphiloop<dphicent and i<10:
+           dphiloop=dphicent
+           center+=delta
+           TS4,dphi4=compute_curve(center,npts=50)
+           dphicent,phimincent,phimaxcent,errorcent=find_dec(TS4,dphi4)
+           i+=1
+           f2.write("dphicent,phimincent,phimaxcent,dphiloop=%.2f\t%.2f\t%.2f\t%.2f\n"%(dphicent,phimincent,phimaxcent,dphiloop))
+           
+       print "Best fit position :\n\tcenter=%.2f\n\twidth=%.2f\n\tmin=%.2f\n\tmax=%.2f\n"%((phimincent+dphicent/2.0)%1,dphicent,phimincent,phimaxcent)
+       f2.write("Best fit position :\n\tcenter=%.2f\n\twidth=%.2f\n\tmin=%.2f\n\tmax=%.2f\n"%((phimincent+dphicent/2.0)%1,dphicent,phimincent,phimaxcent))
+
+   f2.close()
+
+   return (phimincent+dphicent/2.0)%1,dphicent,phimincent,phimaxcent
+
+
+npts=50
+cent,dphi,phimin,phimax=find_center(phi_center,npts)
