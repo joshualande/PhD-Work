23770637913c128de346343183c4ac7016ed5ee4
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2011-09-25 04:35:42 +0000

    nice

--- green_catalog/2FGL/v2/setup_snr.py
+++ green_catalog/2FGL/v2/setup_snr.py
@@ -6,12 +6,15 @@ from lande_roi import *
 from green_catalog import GreenCatalog
 from argparse import ArgumentParser
 
-def build_roi(name):
+def build_snr(name, green_catalog, free_radius=5, fit_emin=1e4, fit_emax=1e5):
 
     folder=os.path.join(os.path.expandvars('$GREENCAT'),name,'v1')
 
-    green_cat=GreenCatalog('$FERMI/catalogs/Green_cat.fits')
-    skydir=green_cat.get_skydir(name)
+    snr=yaml.load(green_cat)[name]
+    name=snr['name']
+    skydir=SkyDir(**snr['cel'])
+
+    size=snr['size']
 
     ds=DataSpecification(
         ft1files=[i.strip() for i in open(e('$FERMI/data/2FGL_P7v4/24m_pass7.4_source_z100_t90_cl0.lst')).readlines()],
@@ -25,16 +28,22 @@ def build_roi(name):
             irf        = 'P7SOURCE_V6',
             maxROI     = 10,
             minROI     = 10,
-            event_class = -1)
+            event_class = 0)
+
+
+    diffuse_sources = diffuse_sources = get_default_diffuse(
+        diffdir=os.path.expandvars('$FERMI/diffuse'),
+        gfile='ring_2year_P76_v0.fits',
+        ifile='isotrop_2year_P76_source_v0.txt')
+
+    catalogs = Catalog2FGL('$FERMI/catalogs/gll_psc_v05.fit',
+                           latextdir='$FERMI/extended_archives/gll_psc_v05_templates/'),
 
     roi = sa.roi(
-        catalogs = Catalog2FGL('$FERMI/catalogs/gll_psc_v04.fit',
-                              latextdir='$FERMI/extended_archives/Extended_archive_v10'),
-        diffuse_sources = get_default_diffuse(
-            diffdir=os.path.expandvars('$FERMI/diffuse'),
-            gfile='ring_2year_P76_v0.fits',
-            ifile='isotrop_2year_P76_source_v0.txt'),
-        fit_emin = 1e4, fit_emax = 1e5
+        catalogs = catalogs,
+        diffuse_sources = diffuse_sources,
+        fit_emin = fit_emin, 
+        fit_emax = fit_emax
         )
 
     if not os.path.exists(folder):
