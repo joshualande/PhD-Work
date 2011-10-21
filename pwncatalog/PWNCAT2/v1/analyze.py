099704edf84db2adc7c5ccb151c3173940d68454
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2011-10-21 07:35:55 +0000

    new folder

---
+++ pwncatalog/PWNCAT2/v1/analyze.py
@@ -0,0 +1,197 @@
+#!/usr/bin/env python
+
+# Not entirely sure why, but pyLikelihood
+# will get a swig::stop_iteraiton error
+# unless it is imported first.
+from roi_gtlike import Gtlike
+
+from uw.like.sed_plotter import plot_sed
+
+from setup_pwn import setup_pwn,get_source
+from uw.like.SpatialModels import Gaussian
+from uw.like.roi_tsmap import TSCalc,TSCalcPySkyFunction
+from argparse import ArgumentParser
+from skymaps import SkyImage,SkyDir
+import yaml
+from SED import SED
+
+from toolbag import tolist
+from likelihood_tools import sourcedict,powerlaw_upper_limit, test_cutoff
+from collections import defaultdict
+import numpy as np
+np.seterr(all='ignore')
+
+parser = ArgumentParser()
+parser.add_argument("--pwndata", required=True)
+parser.add_argument("-p", "--pwnphase", required=True)
+parser.add_argument("-n", "--name", required=True, help="Name of the pulsar")
+parser.add_argument("--emin", default=1e2, type=float)
+parser.add_argument("--emax", default=1e5, type=float)
+parser.add_argument("--no-point", default=False, action="store_true")
+parser.add_argument("--no-extended", default=False, action="store_true")
+parser.add_argument("--no-gtlike", default=False, action="store_true")
+parser.add_argument("--no-plots", default=False, action="store_true")
+parser.add_argument("--no-cutoff", default=False, action="store_true")
+parser.add_argument("--no-extension-upper-limit", default=False, action="store_true")
+args=parser.parse_args()
+
+do_point = not args.no_point
+do_extended = not args.no_extended
+do_gtlike = not args.no_gtlike
+do_plots = not args.no_plots
+do_cutoff = not args.no_cutoff
+do_extension_upper_limit = not args.no_extension_upper_limit
+
+
+name=args.name
+emin=args.emin
+emax=args.emax
+
+phase=yaml.load(open(args.pwnphase))[name]['phase']
+
+free_radius=2
+roi=setup_pwn(name,args.pwndata,phase,free_radius=free_radius)
+while len(roi.parameters())>19 and free_radius>0.5:
+    free_radius*=0.9
+    roi=setup_pwn(name,args.pwndata,phase,xml=None,free_radius=free_radius)
+
+
+from modify import modify_roi
+modify_roi(name,roi)
+
+results=r=defaultdict(lambda: defaultdict(dict))
+
+
+def plots(roi, hypothesis, size=5):
+    print 'Making plots for hypothesis %s' % hypothesis
+    roi.plot_tsmap(filename='residual_tsmap_%s_%s.png' % (hypothesis,name), size=size, pixelsize=0.1)
+    for pixelsize in [0.1,0.25]:
+        roi.plot_counts_map(filename="counts_%.g_%s_%s.png"%(pixelsize,hypothesis,name),
+                            countsfile="counts_%.g_%s_%s.fits"%(pixelsize,hypothesis,name),
+                            modelfile="model_%.g_%s_%s.fits"%(pixelsize,hypothesis,name),
+                            pixelsize=pixelsize,size=size)
+    roi.zero_source(which=name)
+    roi.plot_tsmap(filename='source_tsmap_%s_%s.png' % (hypothesis, name), size=size, pixelsize=0.1)
+    for pixelsize in [0.1,0.25]:
+        roi.plot_counts_map(filename="counts_excess_%.g_%s_%s.png"%(pixelsize,hypothesis,name),
+                            countsfile="counts_excess_%.g_%s_%s.fits"%(pixelsize,hypothesis,name),
+                            modelfile="model_%.g_%s_%s.fits"%(pixelsize,hypothesis,name),
+                            pixelsize=pixelsize,size=size)
+    roi.unzero_source(which=name)
+
+    roi.plot_source(which=name,filename='source_%s_%s.png' % (hypothesis, name), 
+                    size=size, label_psf=False)
+    roi.plot_sources(which=name,filename='sources_%s_%s.png' % (hypothesis, name), 
+                     size=size, label_psf=False)
+
+    roi.toRegion('Region_file_%s.reg'%name)
+    roi.toXML(filename="srcmodel_res_%s_%s.xml"%(hypothesis, name))
+    roi.plot_slice(which=name,filename="outslice_%s_%s.png"%(hypothesis, name),datafile='slice_points_%s_%s.out'%(hypothesis, name))
+    #plot_all_seds(roi, filename="allsed_%s_%s.png"%(hypothesis, name))
+    roi.plot_counts_spectra(filename="Spectra_%s_%s.png"%(hypothesis, name))
+
+
+def pointlike_analysis(roi, hypothesis, upper_limit=False, localize=False, fit_extension=False, extension_upper_limit=False, cutoff=False):
+    print 'Performing Pointlike analysis for %s' % hypothesis
+
+    print_summary = lambda: roi.print_summary(galactic=True)
+    print_summary()
+
+    print roi
+
+    def fit():
+        """ Convenience function incase fit fails. """
+        try:
+            roi.fit(method="minuit",use_gradient=True)
+        except Exception, ex:
+            print 'ERROR spectral fitting: ', ex
+        print_summary()
+
+    fit()
+
+    if localize:
+        try:
+            roi.localize(name, update=True)
+        except Exception, ex:
+            print 'ERROR localizing: ', ex
+        fit()
+
+    if fit_extension:
+        try:
+            roi.fit_extension(name)
+            roi.localize(name, update=True)
+        except Exception, ex:
+            print 'ERROR localizing: ', ex
+        fit()
+
+    p = sourcedict(roi, name)
+
+    if extension_upper_limit:
+        print 'Calculating extension upper limit'
+        p['extension_upper_limit']=roi.extension_upper_limit(which=name, confidence=0.95, spatial_model=Gaussian(), npoints=10)
+
+    if upper_limit:
+        p['upper_limit'] = powerlaw_upper_limit(roi, name, emin=emin, emax=emax, cl=.95)
+    if cutoff:
+        p['test_cutoff']=test_cutoff(roi,name)
+
+    roi.plot_sed(which=name,filename='sed_pointlike_%s_%s.pdf' % (hypothesis,name), use_ergs=True)
+ 
+    roi.save('roi_%s_%s.dat' % (hypothesis,name))
+
+    if do_plots: plots(roi, hypothesis)
+    return p
+
+def gtlike_analysis(roi, hypothesis, upper_limit=False, cutoff=False):
+    print 'Performing Gtlike crosscheck for %s' % hypothesis
+
+    gtlike=Gtlike(roi)
+    like=gtlike.like
+    like.fit(covar=True)
+
+    r=sourcedict(like, name)
+
+    if upper_limit:
+        r['upper_limit'] = powerlaw_upper_limit(like, name, emin=emin, emax=emax, cl=.95)
+    
+    if cutoff:
+        r['test_cutoff']=test_cutoff(like,name)
+
+    for kind, kwargs in [['4bpd',dict(bin_edges=np.logspace(2,5,13))],
+                         ['1bpd',dict(bin_edges=np.logspace(2,5,4))]]:
+
+        print 'Making %s SED' % kind
+        sed = SED(like, name, **kwargs)
+        sed.plot('sed_gtlike_%s_%s_%s.png' % (kind,hypothesis,name)) 
+        sed.verbosity=True
+        sed.save('sed_gtlike_%s_%s_%s.dat' % (kind,hypothesis,name))
+
+    return r
+    
+def save_results(): 
+    open('results_%s.yaml' % name,'w').write(
+        yaml.dump(tolist(results)))
+
+r['at_pulsar']['pointlike']=pointlike_analysis(roi, 'at_pulsar', upper_limit=True, cutoff=do_cutoff)
+save_results()
+if do_gtlike: r['at_pulsar']['gtlike']=gtlike_analysis(roi, 'at_pulsar', upper_limit=True, cutoff=do_cutoff)
+
+if do_point:
+    r['point']['pointlike']=pointlike_analysis(roi, 'point', localize=True, cutoff=do_cutoff, extension_upper_limit=do_extension_upper_limit)
+    save_results()
+    if do_gtlike: r['point']['gtlike']=gtlike_analysis(roi, 'point', cutoff=do_cutoff)
+
+if do_extended:
+    roi.del_source(name)
+    roi.add_source(get_source(name,args.pwndata, extended=True))
+
+    r['extended']['pointlike']=pointlike_analysis(roi, 'point', cutoff=do_cutoff, fit_extension=True)
+    save_results()
+    if do_gtlike: r['extended']['gtlike']=gtlike_analysis(roi, 'point', cutoff=do_cutoff)
+
+    for which in ['pointlike','gtlike']:
+        results['extended'][which]['ts_ext'] = \
+                2*(results['extended'][which]['logLikelihood'] - \
+                   results['point'][which]['logLikelihood'])
+
+save_results()
---
+++ pwncatalog/PWNCAT2/v1/loop_pwn.py
@@ -0,0 +1,88 @@
+"""
+A script to loop over the PWN when
+doing analysis. Example:
+
+python loop_pwn.py -c analyze_v1.py \
+        --pwndata /afs/slac/g/glast/users/rousseau/svn/pwncatalog/1FGL_reanalysis/v4/pwndata_v1.yaml  \
+        --pwnphase /afs/slac/g/glast/users/rousseau/svn/pwncatalog/1FGL_reanalysis/v4/pwnphase_v1.yaml \
+        -o /afs/slac/g/glast/users/rousseau/PWN_cat/1FGL_reanalysis/v4/analyze_v1/
+
+python loop_pwn.py -c analyze.py \
+        --pwndata /u/gl/lande/svn/lande/trunk/pwncatalog/1FGL_reanalysis/v4/pwndata/pwndata_v1.yaml  \
+        --pwnphase /u/gl/lande/svn/lande/trunk/pwncatalog/1FGL_reanalysis/v4/pwndata/pwnphase_v1.yaml \
+        -o /nfs/slac/g/ki/ki03/lande/pwncatalog/1FGL_reanalysis/v4/analyze_v1
+
+"""
+import yaml
+from os.path import expandvars as e
+from os.path import join
+import os
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument("-c", "--command", required=True)
+parser.add_argument("--pwndata", required=True)
+parser.add_argument("-o", "--outdir", required=True)
+args,remaining_args = parser.parse_known_args()
+
+outdir=args.outdir
+
+sources=yaml.load(open(args.pwndata))
+
+if os.path.exists(outdir):
+    raise Exception("outdir %s already exists" % outdir)
+os.makedirs(outdir)
+
+for name in sources.keys():
+    print name
+
+    folder=join(outdir,name)
+    os.makedirs(folder)
+
+    file=join(folder,'run_%s.sh' % name)
+
+    temp=open(file,'w')
+    temp.write("""\
+python %s/%s \\
+-n %s \\
+--pwndata %s %s""" % (os.getcwd(),args.command,name,
+                  args.pwndata,' '.join(remaining_args)))
+
+submit_all=join(outdir,'submit_all.py')
+temp=open(submit_all,'w')
+temp.write("""
+#!/usr/bin/env python
+import glob
+from os.path import exists,isdir,expandvars
+from os import system
+import argparse
+import subprocess
+
+parser=argparse.ArgumentParser()
+parser.add_argument("-n",default=False,action="store_true",help="Don't do anything")
+parser.add_argument("-q","--queue",default='xxl',help="Don't do anything")
+parser.add_argument("-emin", "--emin",  default=1.0e2, type=float)
+parser.add_argument("-emax", "--emax",  default=1.0e5, type=float)
+args = parser.parse_args()
+
+p=subprocess.Popen(['bjobs', '-w'],stdout=subprocess.PIPE)
+queue_jobs,err=p.communicate()
+
+for name in glob.iglob("*"):
+    if isdir(name):
+        results="%s/results*%s.yaml" % (name,name)
+        log="%s/log_%s.txt" % (name,name)
+        run='$PWD/%s/run_%s.sh' % (name,name)
+
+        results_exists = len(glob.glob(results)) > 0
+        is_in_queue = expandvars(run) in queue_jobs
+
+        if not is_in_queue and not results_exists:
+            string="cd %s; bsub -q %s -oo log_%s.txt sh $PWD/run_%s.sh; cd .." % (name,args.queue,name,name)
+            if args.n:
+                print string
+            else:
+                system(string)
+""")
+
+
