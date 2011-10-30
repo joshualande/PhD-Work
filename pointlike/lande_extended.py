cbc8906e2a0c4e68d4e2eab81812784dd93ca652
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2011-10-30 23:46:30 +0000

    new code to make profiles

--- pointlike/lande_extended.py
+++ pointlike/lande_extended.py
@@ -1,118 +1,127 @@
+""" 
+    Code to deal with extended sources in pointlike.
+"""
+import numpy as np
+import yaml
 
-def plot_profile(self,which,filename="profile.png", datafile="profile.yaml", figsize=(4,4),**kwargs):
+from uw.like.roi_plotting import DegreesFormatter
+from uw.like.roi_state import PointlikeState
+from uw.utilities import keyword_options
 
-    extension_list,TS_spectral,TS_bandfits = \
-            self.extension_profile(which=which,filename=datafile,**kwargs)
+from toolbag import tolist
+import pylab as P
 
-    P.figure(figsize=figsize)
-    P.clf()
-    P.plot(extension_list,TS_spectral)
-    P.xlabel('extension (degrees)')
-    P.ylabel('Source TS')
 
-    source=self.get_source(which)
-    P.title('Extension profile %s' % source.name)
+class ExtensionProfile(object):
+    defaults = (
+        ("num_points",      15, "Number of poitns to calcualte profile over"),
+        ("lower_limit",   None, "smallest sigma for the profile. Default is very smalle xtension."),
+        ("upper_limit",   None, "largest sigma for the profile"),
+        ("quick",         None, "Default way to calculate TS. Default is taken from roi.TS()"),
+        ("use_gradient",  None, "use analytic gradient during spectral fit. Default is taken from roi.fit()."),
+        ("fignum",        None, "passed to matplotlib."),
+        ("figsize",      (4,4), "size of plot, in inches."),
+    )
 
-    P.savefig(filename)
+    @keyword_options.decorate(defaults)
+    def __init__(self,roi,which,**kwargs):
+        """ Object for calculating TS as a function of sigma. """
 
-def extension_profile(roi,which,filename='profile.yaml',
-        num_points=15,lower_limit=None,upper_limit=None,
-        quick=True,use_gradient=True):
-    """ Perform a scan in extension. 
 
-        which is the source to calculate the profile of. Note that extension
-        profiles can only be calculated for sources. """
+        self.roi = roi
+        self.which = which
 
-    manager,index=roi.mapper(which)
+        keyword_options.process(self, kwargs)
 
-    if manager != roi.dsm: 
-        raise Exception("An extension profile can only be calculated for diffuse sources.")
-    ds=manager.diffuse_sources[index]
-    esm=manager.bgmodels[index]
+        self.source = roi.get_source(which)
 
-    if not roi.quiet: print 'Calculating extension profile for %s' % ds.name
+        if not hasattr(self.source,'spatial_model'):
+            raise Exception("An extension profile can only be calculated for extended sources")
 
-    if not isinstance(ds,ExtendedSource): 
-        raise Exception("An extension profile can only be calculated for extended sources")
+        self.spatial_model=self.source.spatial_model
+        if not len(self.spatial_model.p)==3: 
+            raise Exception("An extension profile can only be calculated for extended sources with 3 parameters (position + one extension)")
 
-    sm=ds.spatial_model
-    if not len(sm.p)==3: 
-        raise Exception("An extension profile can only be calculated for extended sources with 3 parameters (position + one extension)")
+        self.fit_kwargs = dict(estimate_errors=False)
+        if self.use_gradient is not None: self.fit_kwargs['use_gradient']=self.use_gradient
 
-    # save spatial parameters
-    old_sm_p    = sm.p.copy()
-    old_sm_cov    = sm.cov_matrix.copy()
+        self.ts_kwargs = dict(which=self.which)
+        if self.quick is not None: self.ts_kwargs['quick']=self.quick
 
-    # save spectral parameters
-    old_roi_p = roi.get_parameters().copy()
+        self.fill()
 
-    # Keep the TS function quiet
-    old_quiet=roi.quiet
-    roi.quiet=True
+    def fill(self):
 
-    p,plo,phi=sm.statistical(absolute=True,two_sided=True)
+        roi = self.roi
 
-    x,y,ext=p
-    upper_limit = min(p[2] + max(3*phi[2],p[2]),3) if upper_limit is None else upper_limit
-    if lower_limit is None:
-        lower_limit = float(upper_limit)/num_points/10.0 # make the bottom point ~ 0.1xfirst point
-    extension_list=np.linspace(lower_limit,upper_limit,num_points).tolist()
+        state = PointlikeState(roi)
 
-    TS_spectral=[]
-    TS_bandfits=[]
+        if not roi.quiet: print 'Calculating extension profile for %s' % self.source.name
 
-    TS_bin={}
-    roi.setup_energy_bands()
-    for band in roi.energy_bands:
-        TS_bin[band.emin,band.emax]=[]
+        init_p = roi.get_parameters().copy()
 
-    if not old_quiet: print '%20s %20s %20s' % ('extension','TS_spectral','TS_bandfits')
-    for i,extension in enumerate(extension_list):
-        sm.set_parameters(p=[x,y,extension],absolute=True)
-        esm.initialize_counts(roi.bands)
-        roi.update_counts()
+        # Keep the TS function quiet
+        old_quiet = roi.quiet
+        roi.quiet=True
 
-        # Here the strategy is to try localizing and then
-        # try localizing again from the starting spectral
-        # value. The best fit from both is kepth. """
-        roi.fit(estimate_errors=False,use_gradient=use_gradient)
-        params=roi.parameters()
-        ll_a=-1*roi.logLikelihood(roi.parameters())
-        roi.update_counts(old_roi_p)
-        roi.fit(estimate_errors=False,use_gradient=use_gradient)
-        ll_b=-1*roi.logLikelihood(roi.parameters())
-        if ll_a > ll_b: roi.update_counts(params)
+        sigma = self.spatial_model['sigma']
+        sigma_err = self.spatial_model.error('sigma')
 
-        TS_spectral.append(float(roi.TS(which=which,quick=True)))
-        TS_bandfits.append(float(roi.TS(which=which,quick=True,bandfits=True)))
-        if not old_quiet: print '[ %20s ] %20g %20g' % (sm.pretty_spatial_string(),TS_spectral[i],TS_bandfits[i])
-        for band in roi.energy_bands: TS_bin[float(band.emin),float(band.emax)].append(float(band.ts))
-    
-    # set back spatial parameters
-    sm.set_parameters(old_sm_p,absolute=False)
-    sm.cov_matrix = old_sm_cov
+        upper_limit = min(sigma + max(3*sigma_err,sigma),3) if self.upper_limit is None else self.upper_limit
 
-    # set back spectral parameters
-    roi.set_parameters(old_roi_p)
-    roi.__set_error__()
+        # make the bottom point ~ 0.1xfirst point
+        lower_limit = float(upper_limit)/self.num_points/10.0 if self.lower_limit is None else self.lower_limit
 
-    # reset counts
+        self.extension_list=np.linspace(lower_limit,upper_limit,self.num_points)
 
-    esm.initialize_counts(roi.bands)
-    roi.__update_state__()
+        self.TS_spectral=np.empty_like(self.extension_list)
+        self.TS_bandfits=np.empty_like(self.extension_list)
 
-    roi.quiet=old_quiet
+        roi.setup_energy_bands()
 
-    save_dict=[]
+        if not old_quiet: print '%20s %20s %20s' % ('sigma','TS_spectral','TS_bandfits')
+        for i,sigma in enumerate(self.extension_list):
+            roi.modify(which=self.which, sigma=sigma)
 
-    save_dict={'extension':extension_list,
-               'TS_spectral':TS_spectral,
-               'TS_bandfits':TS_bandfits,
-               'bins':roi.bin_edges.tolist()}
-    save_dict['TS_bands']=[]
-    for (emin,emax),TS in TS_bin.items():
-        save_dict['TS_bands'].append({'emin':emin,'emax':emax,'TS':TS})
-    file=open(filename,'w')
-    file.write(yaml.dump(save_dict))
+            roi.fit(**self.fit_kwargs)
 
-    return extension_list,TS_spectral,TS_bandfits
+            params=roi.parameters()
+            ll_a=-1*roi.logLikelihood(roi.parameters())
+
+            roi.update_counts(init_p)
+            roi.fit(**self.fit_kwargs)
+            ll_b=-1*roi.logLikelihood(roi.parameters())
+            if ll_a > ll_b: roi.update_counts(params)
+
+            self.TS_spectral[i]=roi.TS(**self.ts_kwargs)
+            self.TS_bandfits[i]=roi.TS(bandfits=True,**self.ts_kwargs)
+
+            if not old_quiet: print 'sigma=%.2f ts_spec=%.1f, ts_band=%.1f' % (sigma, self.TS_spectral[i],self.TS_bandfits[i])
+        
+        state.restore()
+
+
+    def todict(self):
+        d=dict(sigma=self.extension_list,
+               TS_spectral=self.TS_spectral,
+               TS_bandfits=self.TS_bandfits)
+        return tolist(d)
+
+    def save(self, filename):
+        file=open(filename,'w')
+        file.write(yaml.dump(self.todict()))
+
+    def plot(self,filename):
+
+        fig = P.figure(self.fignum,self.figsize)
+        axes = fig.add_subplot(111)
+        P.clf()
+        P.plot(self.extension_list,self.TS_spectral)
+        P.xlabel('Extension')
+        P.ylabel('Test Statistic')
+        P.gca().xaxis.set_major_formatter(DegreesFormatter)
+
+
+        P.title('Extension profile %s' % self.source.name)
+
+        P.savefig(filename)
