5ef700a08ba2a361a9d6f5a59129f059dae03a26
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2011-11-10 04:01:26 +0000

    new cutoff test

--- pointlike/lande_plotting.py
+++ pointlike/lande_plotting.py
@@ -9,6 +9,45 @@ from uw.utilities import keyword_options
 from mpl_toolkits.axes_grid1.axes_grid import ImageGrid
 from uw.like.roi_plotting import ROITSMapPlotter, ROISmoothedSources, ROISmoothedSource
 from uw.like.roi_state import PointlikeState
+from lande_sed import LandeSED
+
+def plot_gtlike_cutoff_test(cutoff_results, sed_results, filename=None, title=None, 
+                            model_0_kwargs=dict(color='red'),
+                            model_1_kwargs=dict(color='blue'),
+                            sed_kwargs=dict(),
+                           ):
+    """ Plots the cutoff test performed of a spectrum using the function
+        likelihood_tools.gtlike_test_cutoff.
+
+        Input:
+            cutoff_dict: created by likelihood_tools.gtlike_test_cutoff
+            sed_dict: created by LandeSED.todict(). Can also be a yaml
+              file created by LandeSED.save().
+
+            model_0_kwargs: kwargs for model_0's plot 
+            model_1_kwargs: kwargs for model_0's plot 
+            sed_kwargs: kwargs to pass into LandeSED
+              E.G. flux_units, flux_units, figsize, ...
+    """
+    sed=LandeSED(sed_results,**sed_kwargs)
+
+    axes=sed.plot(plot_spectral_fit=True)
+    axes.autoscale(enable=False)
+
+    model_0 = LandeSED.dict_to_spectrum(cutoff_results['model_0'])
+    model_1 = LandeSED.dict_to_spectrum(cutoff_results['model_1'])
+    sed.plot_spectrum(model_0, **model_0_kwargs)
+    sed.plot_spectrum(model_1, **model_1_kwargs)
+
+    if title is None:
+        axes.set_title('Gtlike Cutoff test for %s' % sed.name)
+    else:
+        axes.set_title(title)
+
+
+    if filename is not None: P.savefig(filename)
+    return axes
+        
 
 class ROIBandPlotter(object):
 
