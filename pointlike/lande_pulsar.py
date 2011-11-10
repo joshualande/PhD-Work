bbf87ce43e67ca080ac586eee524f0dc6cb6db57
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2011-11-10 04:02:57 +0000

    better phaseogram

--- pointlike/lande_pulsar.py
+++ pointlike/lande_pulsar.py
@@ -3,7 +3,7 @@ import numpy as np
 
 from uw.pulsar.lc_plotting_func import PulsarLightCurve
 from uw.pulsar.phase_range import PhaseRange
-def _get_pulsar_data(ft1, phase, radius=1, emin=100, emax=300000):
+def _get_pulsar_data(ft1, radius=1, emin=100, emax=300000):
 
     plc = PulsarLightCurve(ft1, emin=emin, emax=emax, radius=radius)
     plc.fill_phaseogram()
@@ -11,24 +11,29 @@ def _get_pulsar_data(ft1, phase, radius=1, emin=100, emax=300000):
     times = plc.get_times()
     return phases, times
 
-def plot_phaseogram(name, ft1, phase, filename):
+def plot_phaseogram(name, ft1, filename, title=None, off_pulse=None):
     """ Simple code to plot a phaseogram. """
-    phases, times = _get_pulsar_data(ft1, phase)
+    phases, times = _get_pulsar_data(ft1)
 
     nbins=50
     fig = P.figure(None, figsize=(5,5))
     axes = fig.add_subplot(111)
     axes.hist(phases,bins=np.linspace(0,1,nbins+1),histtype='step',ec='red',normed=True,lw=1)
     axes.set_xlim(0,1)
-    axes.set_title(name)
+
+    if title is None: title = 'Phaseogram for %s' % name
+    axes.set_title(title)
+
     axes.set_xlabel('phase')
 
-    PhaseRange(phase).axvspan(axes=axes, label='pwncat1', alpha=0.25, color='green')
+    if off_pulse is not None:
+        PhaseRange(off_pulse).axvspan(axes=axes, alpha=0.5, color='blue')
     P.savefig(filename)
+    return axes
 
-def plot_phase_vs_time(name, ft1, phase, filename):
+def plot_phase_vs_time(name, ft1, filename, title=None, off_pulse=None):
     """ Simple code to plot phase vs time. """
-    phases, times = _get_pulsar_data(ft1, phase)
+    phases, times = _get_pulsar_data(ft1)
 
     # here, put a 2d histogram
     fig = P.figure(None, figsize=(5,5))
@@ -41,8 +46,14 @@ def plot_phase_vs_time(name, ft1, phase, filename):
     extent = [xedges[0], xedges[-1], yedges[0], yedges[-1] ]
     axes.imshow(hist.T,extent=extent,interpolation='nearest',origin='lower', aspect='auto')
 
+    if off_pulse is not None:
+        PhaseRange(off_pulse).axvspan(axes=axes, alpha=0.5, color='white')
+
     axes.set_xlabel('phase')
     axes.set_ylabel('MJD')
-    axes.set_title(name)
+
+    if title is None: title = 'Phase vs Time for %s' % name
+    axes.set_title(title)
 
     P.savefig(filename)
+    return axes
