22d392667805cc94c59f37f5f5a4875e932bb816
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2011-11-17 03:18:51 +0000

    failed attempt at IC code

--- sed_fitter/sed_fitter.py
+++ sed_fitter/sed_fitter.py
@@ -1,9 +1,12 @@
 """ sed_fitter.py
 
-    Code to compute SEDs. This code differs
-    from other SED packages in valuing readability
-    over efficiency. Anything that is supposed
-    to be fast should probably be rewritten in c.
+    Code to compute SEDs. This code differs from other SED packages in
+    valuing human readability over computational efficiency. 
+
+
+    Notes: 
+    
+        * R&L is Rybicki and Lightman "Radiative Processes in Astrophysics
 
 
     This file is free software; you can redistribute it and/or modify
@@ -22,13 +25,15 @@
     Author: Joshua Lande
 """
 
-from math import pow
+import math
 from abc import abstractmethod
 
 import pylab as P
 import numpy as np
 from scipy import integrate 
 from scipy import special
+
+import sympy
 
 import lande_units as u
 
@@ -88,18 +93,28 @@ class Spectrum(object):
         """ Returns dN/dE. Energy must be in erg. """
         pass
 
-    @staticmethod
-    @abstractmethod
-    def units_string():
-        """ Returns a string of the units of the output. """
-        pass
-
-    @classmethod
-    def units(cls):
-        """ Returns the units that __call__ is assumed to be in. """
-        return u.fromstring(cls.units_string())
-
     def __call__(self,*args,**kwargs): self(*args,**kwargs)
+
+
+def uintegrate(func, xmin, xmax, x_units, y_units, e_weight=0):
+    """ Hide the ungliness of not being able to compute
+        integrals of functions with units.
+
+        Here, we assume that func takes in a value with units x_units
+        and returns a number iwth untis y_units. We integrate from
+        xmin to xmax, which both have units x_units and the integral
+        is performed while weighting the function by a weight e_weight. """
+    if xmin not in [0,-np.inf,np.inf]: xmin=float(xmin/x_units)
+    if xmax not in [0,-np.inf,np.inf]: xmax=float(xmax/x_units)
+    f=lambda x: x**e_weight*float(func(x*x_units)/y_units)
+    return (x_units**(e_weight+1)*y_units)*\
+            integrate.quad(f, xmin, xmax)[0]
+
+def ulogspace(emin, emax, num, units):
+    x=np.logspace(np.log10(float(emin/units)), 
+                  np.log10(float(emax/units)), 
+                  num),
+    return u.tosympy(x,units)
 
 
 class ParticleSpectrum(Spectrum):
@@ -109,31 +124,25 @@ class ParticleSpectrum(Spectrum):
     """
 
     def __init__(self,total_energy):
+        """ Normalize total energy output. """
         self.norm = 1
+        self.norm= total_energy/uintegrate(self, 0*u.erg, np.inf*u.erg, x_units=u.erg, y_units=1, e_weight=1)
 
-        # Normalize total energy output
-        power = integrate.quad(lambda e: e*self(e), 0, np.inf)[0]
-        self.norm= float(total_energy/self.energy())
-
-    def energy(self, emin=0*u.erg, emax=np.inf*u.erg):
-        integral=integrate.quad(lambda e: e*self(e), float(emin/u.erg), float(emax/u.erg))[0]
-        return integral*u.erg**2*self.units()
-        
-    @staticmethod
-    def units_string(): return '1/erg'
-
+    def integrate(self, emin=0, emax=np.inf, **kwargs):
+        return uintegrate(self, emin, emax, x_units=u.erg, y_units=1/u.erg, **kwargs)
 
 class PowerLawCutoff(ParticleSpectrum):
 
     def __init__(self, total_energy, index, e_cutoff):
         self.index = index
-        self.e_cutoff = float(e_cutoff/u.erg)
-        self.e_break = float(e_break/u.erg)
-        self.e_scale = float(u.GeV/u.erg)
+        self.e_cutoff = e_cutoff
+        self.e_break = e_break
+        self.e_scale = u.GeV
         super(PowerLawCutoff,self).__init__(total_energy)
 
     def __call__(self, energy):
-        return self.norm*(energy/self.e_scale)**(-self.index)*np.exp(-energy/self.e_cutoff)
+        """ Returns number of particles per unit energy [1/energy]. """
+        return self.norm*(energy/self.e_scale)**(-self.index)*sympy.exp(-energy/self.e_cutoff)
 
 class SmoothBrokenPowerLaw(ParticleSpectrum):
     """ A smoothed broken power-law particle distribution.
@@ -143,12 +152,13 @@ class SmoothBrokenPowerLaw(ParticleSpectrum):
     """
     def __init__(self, total_energy, index1, index2, e_scale, e_break):
         self.index = index
-        self.e_cutoff = float(e_cutoff/u.erg)
-        self.e_break = float(e_break/u.erg)
-        self.e_scale = float(e_scale/u.erg)
+        self.e_cutoff = e_cutoff
+        self.e_break = e_break
+        self.e_scale = e_scale
         super(PowerLawCutoff,self).__init__(total_energy)
 
     def __call__(self, energy):
+        """ Returns number of particles per unit energy [1/energy]. """
         return self.norm*(energy/self.e_scale)**(-self.index1)*(1 + (energy/self.e_break)**beta)*(-(index2-index1)/beta)
 
 class BrokenPowerLawCutoff(ParticleSpectrum):
@@ -157,13 +167,14 @@ class BrokenPowerLawCutoff(ParticleSpectrum):
 
         self.index1 = index1
         self.index2 = index2
-        self.e_cutoff = float(e_cutoff/u.erg)
-        self.e_break = float(e_break/u.erg)
+        self.e_cutoff = e_cutoff
+        self.e_break = e_break
+        self.e_scale = u.eV
         super(BrokenPowerLawCutoff,self).__init__(total_energy)
 
     def __call__(self, energy):
-        """ Return number of particles per unit enert [1/erg]. """
-        return self.norm*(energy**-self.index1)/(1.+(energy/self.e_break)**(-self.index1+self.index2))*np.exp(-energy/self.e_cutoff)
+        """ Return number of particles per unit energy [1/energy]. """
+        return self.norm*((energy/self.e_scale)**-self.index1)/(1.+(energy/self.e_break)**(-self.index1+self.index2))*sympy.exp(-energy/self.e_cutoff)
 
 class ThermalSpectrum(Spectrum):
     def __init__(self, **kwargs):
@@ -179,34 +190,30 @@ class ThermalSpectrum(Spectrum):
         else: raise Exception("Either kT or T must be passed into ThermalSpectrum")
         if len(kwargs)>0: raise Exception("Invalid argument(s) to ThermalSpectrum: %s" % str(kwargs))
 
-        self.pref = 8*np.pi/(u.planck**3*u.speed_of_light**3)
+        self.pref = 8*math.pi/(u.planck**3*u.speed_of_light**3)
 
     @staticmethod
     def occupation_number(x):
         """ This is equation 1.49 in R&L. """
-        return 1/(np.exp(x)-1)
+        return 1/(sympy.exp(x)-1)
 
     def __call__(self, energy):
         """ Return the energy density in units of 1/energy/Volume."""
-        return self.pref*energy**2*ThermalSpectrum.occupation_number(float(energy/self.kT))
+        return self.pref*energy**2*self.occupation_number(energy/self.kT)
 
-    def integrate(self, emin, emax, e_weight=0):
+    def integrate(self, emin, emax, **kwargs):
         """ Integrate the thermal spectrum from emin to emax.
             
             Integrand is in units of energy/volume
         
-            Implementation note: integrate after performing
-            the change of variables (x = energy/kT)
+            Implementation note: integrate with energy measured in units of kT
 
             # note the E^3 dE -> kT^3 x^2 dx
         """
-        xmin,xmax = float(emin/self.kT), float(emax/self.kT)
-
-        return self.pref*self.kT**(3+e_weight)*integrate.quad(lambda x: x**(2+e_weight)**ThermalSpectrum.occupation_number(x), xmin, xmax)[0]
+        return uintegrate(self,emin,emax,x_units=self.kT, y_units=self.pref*self.kT**2, **kwargs)
 
 class CMB(ThermalSpectrum):
     def __init__(self): super(CMB,self).__init__(T=2.725*u.kelvin)
-
 
 class SingleElectronSynchrotron(Spectrum):
     """ Assuming that there is a single electron of energy gamma
@@ -224,10 +231,10 @@ class SingleElectronSynchrotron(Spectrum):
 
     """ This is equation 6.31 in R&L. """
 
-    # for some reason, special.kv(5/3,1e10) is NaN, not 0 ???
-    # for now, just clip the function above 1e5 to be 0
     @np.vectorize
     def F(x):
+        """ for some reason, special.kv(5/3,1e10) is NaN, not 0 ???
+            for now, just clip the function above 1e5 to be 0. """
         if x>1e5: return 0
         return x*integrate.quad(lambda j: special.kv(5./3,j),x,np.inf)[0]
     
@@ -239,32 +246,22 @@ class SingleElectronSynchrotron(Spectrum):
         sin_alpha = 1 # ?
         m = u.electron_mass
         c = u.speed_of_light
-        # This is the R&L prefactor (power/unit frequency)
-        self.pref = (np.sqrt(3)/(2*np.pi))*(q**3*B*sin_alpha)/(u.electron_mass*c**2)
+
+        # This is the prefactor from R&L (eq 6.18) - power/unit frequency
+        self.pref = (math.sqrt(3)/(2*math.pi))*(q**3*B*sin_alpha)/(u.electron_mass*c**2)
 
         # convert to (power/unit energy)
         self.pref /= u.planck
-        self.pref = float(self.pref/(u.erg/u.s/u.erg))
 
         self.omega_c = 3*gamma**2*q*B*sin_alpha/(2*m*c)
-        self.omega_c = float(self.omega_c/u.hz)
-
-    @staticmethod
-    def units_string(): 
-        return '1/s/erg'
-
-    @staticmethod
-    def energy_erg_to_angular_frequency_hz(energy_in_erg):
-        """ Formula is from http://en.wikipedia.org/wiki/Photon. """
-        conversion_factor = float((u.erg/u.planck)/u.hz)
-        return energy_in_erg*conversion_factor
+        self.energy_c = u.hbar*self.omega_c
 
     def __call__(self,energy):
-        omega = self.energy_erg_to_angular_frequency_hz(energy)
-        
-        # return photons radiated per unit energy per unit time
-        # by a single electron
-        return (self.pref/energy)*self.F(omega/self.omega_c)
+        """ return photons radiated per unit energy for a single
+            electron [ph/s/photon energy]. """
+        power_per_energy = self.pref*self.F(float(energy/self.energy_c))
+        photons_per_energy = power_per_energy/energy
+        return photons_per_energy
 
 class Synchrotron(Spectrum):
     """ Calculates the syncrotron radiation
@@ -275,18 +272,15 @@ class Synchrotron(Spectrum):
         self.electron_spectrum = electron_spectrum
         self.magnetic_field = magnetic_field
 
-        print 'not sure what the prefactor should be yet'
-
-    def __call__(self, energy):
+    def __call__(self, photon_energy):
         """ Returns number per unit energy dN/dE. """
-        photon_energy = energy
 
-        # return total power per unit energy by convolving energy per electron
-        # by the electron spectrum.
+        """ return total power per unit energy by convolving energy per electron
+            by the electron spectrum. """
 
         def integrand(electron_energy):
             single_electron=SingleElectronSynchrotron(
-                electron_energy = electron_energy*u.erg, 
+                electron_energy = electron_energy, 
                 magnetic_field = self.magnetic_field)
 
             # return [ph/s/photon energy/electron energy] = 
@@ -294,68 +288,111 @@ class Synchrotron(Spectrum):
             return single_electron(photon_energy)*self.electron_spectrum(electron_energy)
 
         # integrate [ph/s/photon energy/electron energy] over electron energies
-        # Returns the photon flux per unit time per unit photon energy
-        return integrate.quad(integrand,0,np.inf)[0]
+        # Returns the photon flux per unit time per unit photon energy [1/s/energy**2]
+        return uintegrate(integrand,0,np.inf, x_units=u.erg, y_units=1/u.seconds/u.erg**2)
+
+class SingleElectronInverseCompton(Spectrum):
+    """ The inverse compton radiation an electron spectrum
+        and photon spectrum. """
+
+    def __init__(self, electron_energy, photon_spectrum):
+        self.electron_energy = electron_energy
+        self.photon_spectrum = photon_spectrum
 
     @staticmethod
-    def units_string():
-        return '1/s/erg'
+    def f(q,gamma_e):
+        """ This is equation 2.48 in Blumenthal & Gould. """
+        return 2*q*sympy.log(q)+(1+2*q)*(1-q) + 0.5*(gamma_e*q)**2*(1-q)/(1+gamma_e*q)
 
+    def __call__(self, scattered_photon_energy):
+        """ Calculates the inverse compton spectrum expected
+            from a sinle electron and an arbitrary photon spectrum. """
+
+        mc2 = u.electron_mass*u.speed_of_light**2
+
+        def integrand(target_photon_energy):
+
+            ee = self.electron_energy
+
+            # forbidden by kinematic concerns
+
+            minmum_photon_energy = .5*(scattered_photon_energy + \
+                  sympy.sqrt(scattered_photon_energy**2+ \
+                             scattered_photon_energy*(mc2)**2/target_photon_energy))
+
+            if ee < minmum_photon_energy: return 0
+
+            gamma_e = 4*target_photon_energy*ee/(mc2)**2
+            q=scattered_photon_energy/(ee*gamma_e*(1-scattered_photon_energy/ee))
+
+            return 2*math.pi*u.r0**2*u.speed_of_light*\
+                    (mc2/target_photon_energy)*\
+                    self.photon_spectrum(target_photon_energy)*\
+                    self.f(q,gamma_e)
+                    
+        # integrate over the photon field
+        return uintegrate(integrand,0,np.inf, x_units=u.eV, y_units=1/u.seconds/u.eV)
 
 class InverseCompton(Spectrum):
 
-    @staticmethod
-    def f(x): 
-        """ This is 7.27 in R&L -- from Blumenthal and Gold, 1970. """
-        return 2*x*np.log(x) + x + 1 - 2*x**2
+    def __init__(self, electron_spectrum, photon_spectrum):
+        self.electron_spectrum = electron_spectrum
+        self.photon_spectrum = photon_spectrum
 
-    def __init__(self, enenergy_spectrum, photon_spectrum):
-        """ The inverse compton radiation an electron spectrum
-            and photon spectrum. """
-        pass
-
-    def __call__(self, energy):
-        photon_energy = energy
+    def __call__(self, photon_energy):
+        """ Returns number per unit energy dN/dE. """
 
         def integrand(electron_energy):
-            pass
+            single_electron=SingleElectronInverseCompton(
+                electron_energy = electron_energy, 
+                photon_spectrum = self.photon_spectrum)
+
+            return single_electron(photon_energy)*self.electron_spectrum(electron_energy)
+
+        # integrate over all electrons
+        return uintegrate(integrand,0,np.inf, x_units=u.eV, y_units=1/u.seconds/u.eV**2)
+
+
+
 
 if __name__ == '__main__':
 
     # all the code below is for testing out the main code.
 
-    def plot_sed(synch,distance):
+    def plot_sed(synch,distance,
+                 x_units = u.eV,
+                 y_units = u.eV/u.cm**2/u.s,
+                ):
         """ Plot the synchotron + inverse compton SED. """
 
         emin=1e-7*u.eV
         emax=1e15*u.eV
 
-        x = np.logspace(np.log10(float(emin/u.erg)), np.log10(float(emax/u.erg)), 100)
+        x = ulogspace(emin, emax, 100, u.eV)
+
+        print 'min',synch(emin)
+        print 'max',synch(emax)
 
         # y is in units of self.units()
-        y = np.asarray(map(synch,x))
-
-        x=u.tosympy(x,u.erg)
-
-        y=u.tosympy(y,synch.units())
+        y = sympy.Matrix(map(synch,x)).transpose()
         y=y.multiply_elementwise(x).multiply_elementwise(x)
-        y /= (4*np.pi*distance**2)
 
-        x_units = u.eV
-        y_units = u.eV/u.cm**2/u.s
-        x = u.tonumpy(x,x_units)
-        y = u.tonumpy(y,y_units)
+        y /= (4*math.pi*distance**2)
 
         fig = P.figure(None,figsize=(5.5,4.5))
         P.clf()
         fig.subplots_adjust(left=0.18,bottom=0.13,right=0.95,top=0.95)
         axes = fig.add_subplot(111)
 
+        x = u.tonumpy(x,x_units)
+        y = u.tonumpy(y,y_units)
         axes.loglog(x,y)
+
+        axes.set_xlim(1e-7,1e15)
+        axes.set_ylim(1e-08,1e-1)
+
         axes.set_xlabel('Energy (eV)')
         axes.set_ylabel(r'E$^2$ dN/dE (eV cm$^{-2}$ s$^{-1}$)')
-        axes.set_xlim(float(emin/x_units),float(emax/x_units))
-        axes.set_ylim(1e-08,1e-1)
 
         P.savefig('sed.png')
 
@@ -367,13 +404,13 @@ if __name__ == '__main__':
                 e_break = 1e7*u.eV,
                 e_cutoff = 1.e14*u.eV)
 
-        print 'total energy %s erg' % float(electron_spectrum.energy()/u.erg)
+        print 'total electron energy %s' % u.repr(electron_spectrum.integrate(e_weight=1),'erg')
         emin=1e-7*u.eV
         emax=1e15*u.eV
 
-        electron_spectrum.loglog(emin=emin, emax=emax, 
-                   e_weight=2, x_units='eV', y_units='eV', 
-                   filename='ElectronSpectrum.png')
+        #electron_spectrum.loglog(emin=emin, emax=emax, 
+        #           e_weight=2, x_units='eV', y_units='eV', 
+        #           filename='ElectronSpectrum.png')
 
         syn = SingleElectronSynchrotron(electron_energy=1e3*u.MeV,
                                         magnetic_field=3e-6*u.gauss)
@@ -381,7 +418,16 @@ if __name__ == '__main__':
         synch = Synchrotron(electron_spectrum=electron_spectrum,
                           magnetic_field=3e-6*u.gauss)
 
-        plot_sed(synch,distance=4.2*u.kpc)
+        cmb=CMB()
+
+        #ic = InverseCompton(electron_spectrum=electron_spectrum,
+        #                    photon_spectrum = cmb)
+
+        ic = SingleElectronInverseCompton(electron_energy=u.TeV,
+                                          photon_spectrum = cmb)
+
+
+        plot_sed(ic,distance=4.2*u.kpc)
 
 
     stefan_hess_j1813()
@@ -390,11 +436,10 @@ if __name__ == '__main__':
         cmb=CMB()
         print 'kt=',cmb.kT
         print 'dnde=',cmb(cmb.kT)
-        total_energy_density = cmb.integrate(0,np.inf*u.eV, e_weight=1)
+        total_energy_density = cmb.integrate(e_weight=1)
         print 'total',total_energy_density
-        print total_energy_density/(u.eV*u.m**-3)
-        print '%.2e eV/m^3' % float(total_energy_density/(u.eV*u.m**-3))
-        print 'cmb = 2.60e5 eV/m^3'
+        print 'computed = ', u.repr(total_energy_density,'eV/m^3')
+        print 'from stefan = 2.60e5 eV/m^3'
 
     #test_thermal_spectrum()
 
