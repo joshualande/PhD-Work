1079152602ae9ff853c393a338b3fa8c4747f5d0
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2011-11-18 05:06:53 +0000

    organizing

---
+++ sed_fitter/sed_ic.py
@@ -0,0 +1,81 @@
+
+class InverseCompton(Spectrum):
+    """ The inverse compton radiation an electron spectrum
+        and photon spectrum. """
+
+    per_decade = 10
+
+    def __init__(self, electron_spectrum, photon_spectrum):
+        self.electron_spectrum = electron_spectrum
+        self.photon_spectrum = photon_spectrum
+
+    @staticmethod
+    def F(q,gamma_e):
+        """ This is equation 2.48 in Blumenthal & Gould. """
+        return 2*q*np.log(q)+(1+2*q)*(1-q) + 0.5*(gamma_e*q)**2*(1-q)/(1+gamma_e*q)
+
+    def spectrum(self, scattered_photon_energy):
+        """ Calculates the inverse compton spectrum expected
+            from a sinle electron and an arbitrary photon spectrum. 
+            
+            Returns [ph/s/scattered photon energy]. """
+
+        mc2 = float(u.electron_mass*u.speed_of_light**2/u.erg)
+
+        def integrand(electron_energy, target_photon_energy):
+            """ return [ph/s/incident photon energy/scattered photon energy] 
+                for a single electron
+                
+                in units [1/s/erg^2]
+            """
+
+            ee = electron_energy
+            c = u.speed_of_light
+            m = u.electron_mass
+
+            electron_gamma = target_photon_energy/mc2
+
+            gamma_e = 4*target_photon_energy*ee/(mc2)**2
+            q=scattered_photon_energy/(ee*gamma_e*(1-scattered_photon_energy/ee))
+
+            # this formula is basically 7.28a in R&L with the difference that
+
+            # C => photon_spectrum
+            # v(energy) => 
+
+            # prefactor has units cm^3*s^-1*erg^-1
+            pref = 2*math.pi*u.r0**2*c*electron_gamma**-2*(target_photon_energy*u.erg)**-1
+            
+            pref = float(pref/(u.cm**3*u.second**-1*u.erg**-1))
+
+            # Note, photon_spectrum has units 'ph/erg/cm^3'
+            # F is unitless
+            # so pref*photon_spectrum*F has units ph/erg/cm^3 * cm^3*s^-1*erg^-1 = ph s^-1 erg^-2
+            return pref*\
+                    self.photon_spectrum(target_photon_energy, units=False)*\
+                    self.F(q,gamma_e)
+
+        def integrate_over_electron(target_photon_energy):
+            """ Integrate the InverseCompton spectrum over the photon electron distribution.
+            
+                returns [ph/s/incident photon energy/scattered photon energy] """
+            
+            f=lambda electron_energy: integrand(electron_energy,target_photon_energy)*\
+                    self.electron_spectrum(electron_energy, units=False)
+
+            emin = self.electron_spectrum.emin
+            emax = self.electron_spectrum.emax
+
+            return logsimps(f,emin,emax,self.per_decade)
+        # would be nice to vectorize both integrals =(
+        integrate_over_electron = np.vectorize(integrate_over_electron)
+
+
+        emin = self.photon_spectrum.emin
+        emax = self.photon_spectrum.emax
+
+        return logsimps(integrate_over_electron,emin,emax,self.per_decade)
+
+    @staticmethod
+    def units_string(): return '1/erg/s'
+
---
+++ sed_fitter/sed_synch.py
@@ -0,0 +1,84 @@
+
+class Synchrotron(Spectrum):
+    """ Calculates the syncrotron power radiated
+        by a spectrum of electrons in a magnetic field. 
+
+        This treatment follows section 6.2 in R&L and especially  
+        the formulas 6.17c, 6.18, and 6.31c
+    
+        The spectrum is in unit of energy per unit time
+        per unit frequency emitted by a single electron.
+        """
+
+    """ This is equation 6.31 in R&L. """
+
+    per_decade = 10
+
+    @np.vectorize
+    def F(x):
+        """ for some reason, special.kv(5/3,1e10) is NaN, not 0 ???
+            for now, just clip the function above 1e5 to be 0. 
+
+            N.B very important to always return a float since the type of
+            the entire return array returned when a vector is passed in
+            is decided by the type of the first number that is returned.
+
+            See http://docs.scipy.org/doc/numpy-1.6.0/reference/generated/numpy.vectorize.html """
+        if x>1e5: return float(0)
+        return x*integrate.quad(lambda j: special.kv(5./3,j),x,np.inf)[0]
+
+    def __init__(self, electron_spectrum, magnetic_field):
+
+        self.electron_spectrum = electron_spectrum
+
+        q = u.electron_charge
+        B = magnetic_field
+        sin_alpha = 1 # ?
+        print 'what to do about sin(alpha)???'
+        m = u.electron_mass
+        c = u.speed_of_light
+
+
+        # This is the prefactor from R&L (eq 6.18) - power/unit frequency
+        self.pref = (math.sqrt(3)/(2*math.pi))*(q**3*B*sin_alpha)/(u.electron_mass*c**2)
+        # convert from (power/frequency) to (power/energy) in units of erg/s/erg (or 1/s)
+        self.pref /= u.planck
+        self.pref = float(self.pref/(u.erg*u.second**-1*u.erg**-1))
+
+        # This is formula 6.17 in R&L except for the gamma**2
+        omega_c = 3*q*B*sin_alpha/(2*m*c)
+        self.energy_c_pref = float(u.hbar*omega_c/u.erg)
+
+        self.mc2 = m*c**2
+        self.mc2_in_erg = float(self.mc2/u.erg)
+
+    def spectrum(self, photon_energy):
+        """ return total power per emitted per unit energy by
+            the spectrum of electrons.
+            
+            Integrate the power by a single electron over the spectrum of electrons. """
+
+        def integrand(electron_energy):
+            """ Integrate over electron gamma: unitless =). """
+            electron_gamma = electron_energy/self.mc2_in_erg
+
+            energy_c = electron_gamma**2*self.energy_c_pref
+
+            # power_per_energy in units of erg/s/erg
+            power_per_energy = self.pref*self.F(photon_energy/energy_c)
+            # divide by photon energy to get photons/energy for a single
+            # electron (in units of ph/erg/s)
+            photons_per_energy = power_per_energy/photon_energy
+
+            # return [ph/s/photon energy/electron energy] = 
+            #             [ph/s/electron/photon energy]*[number of electrons/electron energy]
+            return photons_per_energy*self.electron_spectrum(electron_energy, units=False)
+
+        # integrate [ph/s/photon energy/electron energy] over electron energies
+        # Returns is photon flux per energy per time [1/erg/s]
+        emin = self.electron_spectrum.emin
+        emax = self.electron_spectrum.emax
+        return logsimps(integrand,emin, emax, per_decade=self.per_decade)
+
+    @staticmethod
+    def units_string(): return '1/erg/s'
