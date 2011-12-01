de1d5318fffa1a09b4879c86ebbbc9eab769f23d
rousseau <rousseau@37a9682d-6443-41a2-8582-b44379b6e86f>
2011-12-01 16:53:55 +0000

    updated all_sed code

--- pointlike/likelihood_tools.py
+++ pointlike/likelihood_tools.py
@@ -1,3 +1,4 @@
+
 """ This file contains various function which I have found useful. """
 import pprint
 from math import ceil
@@ -502,31 +503,74 @@ def test_cutoff(like_or_roi, *args, **kwargs):
 
 def pointlike_plot_all_seds(roi, filename=None, ncols=4, **kwargs):
     """ Create an SED of all sources in the ROI as a big plot. """
-
+    
     sources=roi.get_sources()
     nrows = int(ceil(float(len(sources))/ncols))
+    
+    fig = P.figure(figsize=(2.5*ncols,2*nrows),frameon=False)
+    tit=P.title("All seds of the sources included in the region.\nRed : fitted sources\nBlue : non fitted sources inside the counts map\nBlack : sources outside of the counts map")
 
-    fig = P.figure(figsize=(2.5*ncols,2*nrows))
-
-    grid = AxesGrid(fig, 111, 
+    tit.axes.get_xaxis().set_visible(False)
+    tit.axes.get_yaxis().set_visible(False)
+    grid = AxesGrid(fig, 111,
                     aspect=False,
                     nrows_ncols = (nrows, ncols),
                     axes_pad = 0.1,
                     add_all=True,
                     label_mode = "L")
 
+
+    from celgal import dist
+    from mpl_toolkits.axes_grid.anchored_artists import AnchoredText
+    use_ergs=True
+    energy_flux_unit = None
+    if energy_flux_unit is None:
+        energy_flux_unit = 'erg' if use_ergs else 'MeV'
+    assert energy_flux_unit in ('erg', 'MeV', 'GeV', 'eV') , 'unrecognized energy flux unit'
+    energy_flux_factor = dict(erg=1.602e-6, MeV=1, eV=1e6, GeV=1e-3)[energy_flux_unit]
+    dir=["bottom","top","left","right"]
     for i,which in enumerate(sources):
-        roi.plot_sed(which,axes=grid[i],**kwargs)
+        source=roi.get_source(which=which)
+        distance=dist([source.skydir.ra(),source.skydir.dec()],[roi.sa.roi_dir.ra(),roi.sa.roi_dir.dec()])
+        axis = (80, 5e5, 1e-7*energy_flux_factor,3.0e-4*energy_flux_factor)
+        axes=grid[i]
+        if distance<float(roi.sa.maxROI):
+            if len(source.model.get_parameters())!=0:
+                for axem in dir:
+                    axes.spines[axem].set_color('red')
+            else :
+                for axem in dir:
+                    axes.spines[axem].set_color('blue')
+        else :
+            for axem in dir:
+                axes.spines[axem].set_color('black')
+        at = AnchoredText("%s"%which.name,
+                          prop=dict(size=8), frameon=True,
+                          loc=2,
+                          )
+        at.patch.set_boxstyle("round,pad=0.,rounding_size=0.2")
+        axes.add_artist(at)
+                                
+            
+        roi.plot_sed(which,axes=grid[i],axis=axis,title=which.name,energy_flux_unit=energy_flux_unit,**kwargs)
+        
 
+        
+        
+        
+            
     if filename is not None: P.savefig(filename)
-
+    
 plot_all_seds = pointlike_plot_all_seds # for now
+                                                                
 
 
-def freeze_insignificant_to_catalog(roi, catalog, exclude_names, min_ts=4):
+
+def freeze_insignificant_to_catalog(roi,catalog, exclude_names=[], min_ts=25):
     """ Replace all insigificant 2FGL catalog sources
         with the predictions of 2FGL and 
         the spectral shape of the source frozen. """
+
     for source in roi.get_sources():
 
         if source.name in exclude_names: continue
@@ -538,10 +582,11 @@ def freeze_insignificant_to_catalog(roi, catalog, exclude_names, min_ts=4):
                 pass
             else:
                 model=catalog_source.model
-                model.free[0] = source.model.free[0]
+                model.free[0] = False
                 model.free[1:] = False
                 print 'Freezing spectra of %s to 2FGL prediction' % source.name
-                roi.modify(which=source, model=model, keep_old_flux=False)
+                roi.modify(which=source, model=model,keep_old_flux=False)
+
 
 
 
