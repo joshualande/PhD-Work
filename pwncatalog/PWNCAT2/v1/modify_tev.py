e7c1ecc6a5e56bc5f68810151728e728ee9061fd
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2011-12-05 06:29:05 +0000

    removing source

--- pwncatalog/PWNCAT2/v1/modify_tev.py
+++ pwncatalog/PWNCAT2/v1/modify_tev.py
@@ -11,6 +11,10 @@ def modify_roi(name,roi):
         print 'Deleting MSH15-52 from background model!'
         roi.del_source('MSH15-52') # remove 2FGL version
 
+    if name=='HESS_J1303-631':
+        # not sure if I should be removing this source. -- Dec 4, 2011 Joshua Lande
+        roi.del_source('2FGL J1303.7-6316c')
+
 
 
 
