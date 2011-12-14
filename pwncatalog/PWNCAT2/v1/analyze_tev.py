0c19cdaff94bfeeb3ad10b3e5b44c0a6fc32c2e4
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2011-12-14 14:52:31 +0000

    loop

--- pwncatalog/PWNCAT2/v1/analyze_tev.py
+++ pwncatalog/PWNCAT2/v1/analyze_tev.py
@@ -16,10 +16,15 @@ import numpy as np
 np.seterr(all='ignore')
 
 parser = ArgumentParser()
-parser.add_argument("--tevdata", required=True)
+parser.add_argument("--tevsources", required=True)
 parser.add_argument("-n", "--name", required=True, help="Name of the pulsar")
 parser.add_argument("--emin", default=10**4, type=float)
 parser.add_argument("--emax", default=10**5.5, type=float)
+
+parser.add_argument("--no-at-tev", default=False, action="store_true")
+parser.add_argument("--no-point", default=False, action="store_true")
+parser.add_argument("--no-extended", default=False, action="store_true")
+
 parser.add_argument("--use-gradient", default=False, action="store_true")
 parser.add_argument("--no-gtlike", default=False, action="store_true")
 parser.add_argument("--no-plots", default=False, action="store_true")
@@ -27,6 +32,10 @@ parser.add_argument("--no-upper-limits", default=False, action="store_true")
 parser.add_argument("--no-seds", default=False, action="store_true")
 parser.add_argument("--max-free", default=10, type=float)
 args=parser.parse_args()
+
+do_at_tev = not args.no_at_tev
+do_point = not args.no_point
+do_extended = not args.no_extended
 
 do_gtlike = not args.no_gtlike
 do_plots = not args.no_plots
@@ -41,7 +50,7 @@ emax=args.emax
 
 def get_roi(**kwargs):
     print 'Building the ROI'
-    roi=setup_tev(name,args.tevdata, 
+    roi=setup_tev(name,args.tevsources, 
                   free_radius=5, max_free=args.max_free, 
                   fit_emin=emin, fit_emax=emax, 
                   **kwargs)
@@ -57,9 +66,33 @@ kwargs = dict(name=name, seds=do_seds, emin=emin, emax=emax)
 
 save=lambda:save_results(results,name)
 
-roi=get_roi(extended=True)
-r['at_tev']['pointlike']=pointlike_analysis(roi, hypothesis='at_tev', upper_limit=do_upper_limits, **kwargs)
-save()
-if do_plots: plots(roi, name, 'at_tev', emin, emax)
-if do_gtlike: r['at_tev']['gtlike']=gtlike_analysis(roi, hypothesis='at_tev', upper_limit=do_upper_limits, **kwargs)
-save()
+if do_at_tev:
+    roi=get_roi(extended=True)
+    r['at_tev']['pointlike']=pointlike_analysis(roi, hypothesis='at_tev', upper_limit=do_upper_limits, **kwargs)
+    save()
+    if do_plots: plots(roi, name, 'at_tev', emin, emax)
+    if do_gtlike: r['at_tev']['gtlike']=gtlike_analysis(roi, hypothesis='at_tev', upper_limit=do_upper_limits, **kwargs)
+    save()
+
+if do_point:
+    roi=get_roi(extended=False)
+    r['point']['pointlike']=pointlike_analysis(roi, hypothesis='point', upper_limit=False, localize=True, **kwargs)
+    save()
+    if do_plots: plots(roi, name, 'point', emin, emax)
+    if do_gtlike: r['point']['gtlike']=gtlike_analysis(roi, hypothesis='point', upper_limit=False, **kwargs)
+    save()
+
+if do_extended:
+    roi=get_roi(extended=False)
+    r['extended']['pointlike']=pointlike_analysis(roi, hypothesis='extended', upper_limit=False, fit_extension=True, **kwargs)
+    save()
+    if do_plots: plots(roi, name, 'extended', emin, emax)
+    if do_gtlike: r['extended']['gtlike']=gtlike_analysis(roi, hypothesis='extended', upper_limit=False, **kwargs)
+    save()
+
+    for which in ['pointlike','gtlike']:
+        try:
+            r['extended'][which]['ts_ext'] = \
+                    2*(r['extended'][which]['logLikelihood'] - r['point'][which]['logLikelihood'])
+        except KeyError:
+            pass
