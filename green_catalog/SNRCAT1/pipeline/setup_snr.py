e070f80c8ff77aba26a9d95026e3f0284f38a0f3
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2011-12-20 16:45:05 +0000

    update data in setup

--- green_catalog/SNRCAT1/pipeline/setup_snr.py
+++ green_catalog/SNRCAT1/pipeline/setup_snr.py
@@ -26,10 +26,10 @@ def setup_roi(name, snrdata, free_radius=2, fit_emin=1e4, fit_emax=1e5):
     size=snr['size']
 
     ds=DataSpecification(
-        ft1files=[i.strip() for i in open(e('$FERMI/data/2FGL_P7v4/24m_pass7.4_source_z100_t90_cl0.lst')).readlines()],
-        binfile='$FERMI/data/2FGL_P7v4/binned_24m_pass7.4_source_z100_t90_cl0.fits',
-        ft2files='$FERMI/data/2FGL_P7v4/ft2_2years.fits',
-        ltcube='$FERMI/data/2FGL_P7v4/ltcube_24m_pass7.4_source_z100_t90_cl0.fits')
+        ft1files='$FERMILANDE/data/PWNCAT2/nov_30_2011/ft1_PWNCAT2_allsky.fits',
+        binfile='$FERMILANDE/data/PWNCAT2/nov_30_2011/ft2_PWNCAT2_allsky.fits',
+        ft2files='$FERMILANDE/data/PWNCAT2/nov_30_2011/ltcube_PWNCAT2_allsky.fits',
+        ltcube='$FERMILANDE/data/PWNCAT2/nov_30_2011/binned_%s.fits')
 
     sa=SpectralAnalysis(ds,
             binsperdec = 4,
@@ -41,12 +41,12 @@ def setup_roi(name, snrdata, free_radius=2, fit_emin=1e4, fit_emax=1e5):
 
 
     diffuse_sources = diffuse_sources = get_default_diffuse(
-        diffdir=e('$FERMI/diffuse'),
+        diffdir=e('$FERMILANDE/diffuse'),
         gfile='ring_2year_P76_v0.fits',
         ifile='isotrop_2year_P76_source_v0.txt')
 
-    catalogs = Catalog2FGL('$FERMI/catalogs/gll_psc_v05.fit',
-                           latextdir='$FERMI/extended_archives/gll_psc_v05_templates/',
+    catalogs = Catalog2FGL('$FERMILANDE/catalogs/gll_psc_v05.fit',
+                           latextdir='$FERMILANDE/extended_archives/gll_psc_v05_templates/',
                            free_radius=free_radius)
 
     roi = sa.roi(
