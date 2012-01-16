afb1f57c16b0578492892f39d938778c5e685eb8
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2012-01-16 22:26:15 +0000

    Add

--- pwncatalog/PWNCAT2/v1/modify_psr.py
+++ pwncatalog/PWNCAT2/v1/modify_psr.py
@@ -44,22 +44,55 @@ def modify_roi(name,roi):
             model.set_flux(flux,emin=100,emax=100000)
             roi.modify(which=name,model=model)
 
-    if name=='PSRJ0835-4510':
-        # Vela X is already include in 2FGL and needs to be pruned
-        # out of the background model - Nov, 4, 2011 Joshua Lande
-        print 'Deleting Vela X from background model!'
-        roi.del_source('VelaX')
+    for psr_name, cat_name in [
+        ['PSRJ0007+7303','2FGL J0007.0+7303'],
+        ['PSRJ0030+0451','2FGL J0030.4+0450'],
+        ['PSRJ0034-0534','2FGL J0034.4-0534'],
+        ['PSRJ0106+4855','2FGL J0106.5+4854'],
+        ['PSRJ0205+6449','2FGL J0205.8+6448'],
+        ['PSRJ0218+4232','2FGL J0218.1+4233'],
+        ['PSRJ0248+6021','2FGL J0248.1+6021'],
+        ['PSRJ0340+4130','2FGL J0340.4+4131'],
+        ['PSRJ0357+3205','2FGL J0357.8+3205'],
+        ['PSRJ0437-4715','2FGL J0437.3-4712'],
+        ['PSRJ0534+2200','2FGL J0534.5+2201'],
+        ['PSRJ0610-2100','2FGL J0610.3-2059'],
+        ['PSRJ0613-0200','2FGL J0613.8-0200'],
+        ['PSRJ0614-3329','2FGL J0614.1-3329'],
+        ['PSRJ0622+3749','2FGL J0621.9+3750'],
+        ['PSRJ0631+1036','2FGL J0631.5+1035'],
+        ['PSRJ0633+0632','2FGL J0633.7+0633'],
+        ['PSRJ0633+1746','2FGL J0633.9+1746'],
+        ['PSRJ0659+1414','2FGL J0659.7+1417'],
+        ['PSRJ0729-1448','?']
+        ['PSRJ0734-1559','2FGL J0734.6-1558'],
+        ['PSRJ0742-2822','2FGL J0742.4-2821'],
+        ['PSRJ0751+1807','2FGL J0751.1+1809'],
 
-    if name=="PSRJ1513-5908":
+        # Vela X is already include in 2FGL and needs to be pruned out
+        # of the background model - Nov, 4, 2011 Joshua Lande
+        ['PSRJ0835-4510',['2FGL J0835.3-4510','VelaX']], 
+
+        ['PSRJ0908-4913','2FGL J0908.5-49'],
+        ['PSRJ0940-5428',[]], # no 2FGL source...
+        ['PSRJ1016-5857','2FGL J1016.5-5858'],
+
+        ['PSRJ1023-5746','2FGL J1023.5-5749c'],
+
         # MSH 15-52 is already included in the 2FGL with a template
         # we nee do delete it from the source model - Dec 09 Romain
-        print 'Deleting MSH 15-52 from the background model !'
-        roi.del_source("MSH15-52")
+        ['PSRJ1513-5908','MSH15-52'],
+    ]:
+
+        if name == psr_name: 
+            roi.del_source(cat_name)
+        
+
+
 
     #Romain Dec 21 - had problems with a source associated with 1023 which is not removed. Actually two sources are associated to 1023 in the 2FGL
     #Change a source into power law cause it seems to me that it should be a power law at least in the off
     if name=="PSRJ1023-5746":
-        roi.del_source("2FGL J1023.5-5749c")
         from uw.like.Models import PowerLaw
         roi.modify(which='2FGL J1045.0-5941', model=PowerLaw(norm=1e-11, index=2), keep_old_flux=True)
         
