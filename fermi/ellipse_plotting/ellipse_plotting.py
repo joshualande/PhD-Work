2a5e3f5891ea7d525ad7f25058f427e1194924bd
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2012-01-18 02:53:39 +0000

    update

--- fermi/ellipse_plotting/ellipse_plotting.py
+++ fermi/ellipse_plotting/ellipse_plotting.py
@@ -1,19 +1,24 @@
 import pyfits, pywcsgrid2
 import matplotlib.pyplot as plt
 import numpy as np
-from pylab import savefig, suptitle, figure
+import pylab
 from skymaps import SkyDir
 from uw.utilities import region_writer
 import pyregion
 import pywcs
 
+
 IMAGE_SIZE = 30
 
-
 def angle_equatorial_from_galactic(center, angle_galactic):
+    """ For a given position in the sky,
+        and a given angle defined as a rotation east of galactic
+        north, comute the angle defined as a rotation east of
+        celestial north. """
     l,b=center.l(),center.b()
     ra,dec=center.ra(),center.dec()
 
+    # define a galacitc projection coordiante system centered on our position in the sky
     wcs = pywcs.WCS(naxis=2)
 
     # n.b. (l,b) has an image coordiante of (0,0), which is convenient
@@ -22,6 +27,7 @@ def angle_equatorial_from_galactic(center, angle_galactic):
     wcs.wcs.crval = [l, b]
     wcs.wcs.ctype = ["GLON-ZEA", "GLAT-ZEA"]
 
+    # compute the axes of increasing 'dec' and 'b'
 
     gal_axis = wcs.wcs_sky2pix(np.array([[l,b + 0.01]], float),1)[0]
     # find the image vector of increasing galactic latitude
@@ -32,9 +38,20 @@ def angle_equatorial_from_galactic(center, angle_galactic):
     cel_axis = wcs.wcs_sky2pix(np.array([[cel_dir.l(),cel_dir.b()]], float),1)[0]
     cel_axis /= np.sqrt(cel_axis.dot(cel_axis))
 
-    rotation = np.degrees(np.arccos(cel_axis.dot(gal_axis)))
+    # compute the angle between the two axes
 
-    return angle_galactic - rotation
+    # N.B. use compute angle of each axes using atan2 to get correct coordinate.
+    # The difference of these two angles should be the absolute angle
+    # with the correct minus sign.
+    rotation = np.degrees(
+        np.arctan2(gal_axis[1],gal_axis[0]) - np.arctan2(cel_axis[1],cel_axis[0])
+    )
+
+    # the new angle is just the old angle + the difference between axes
+
+    # The + sign was determined emperically
+    angle_equatorial = angle_galactic +  rotation
+    return angle_equatorial 
 
 
 def new_angle(center, angle):
@@ -48,7 +65,7 @@ def new_angle(center, angle):
 
     
 
-def gal_header(center, galactic=True):
+def gal_header(center, major_axes, galactic=True):
 
     values = [
             ["NAXIS",  2,          ],
@@ -65,8 +82,8 @@ def gal_header(center, galactic=True):
             ["CRVAL1", center.l() if galactic else center.ra(),        ],
             ["CRVAL2", center.b() if galactic else center.dec(),       ],
 
-            ["CDELT1", 0.1,       ],
-            ["CDELT2", -0.1,        ],
+            ["CDELT1", -3.*major_axes/IMAGE_SIZE,       ],
+            ["CDELT2", 3.*major_axes/IMAGE_SIZE,        ],
     ]
 
     if galactic is False:
@@ -82,12 +99,12 @@ def gal_header(center, galactic=True):
 
     return header
 
-equ_header = lambda center: gal_header(center, galactic=False)
+equ_header = lambda *args: gal_header(*args, galactic=False)
 
-def draw_gal(center):
+def draw_gal(center, major_axes):
     ra,dec=center.ra(),center.dec()
 
-    header=gal_header(center)
+    header=gal_header(center, major_axes)
 
     global gal_ax
 
@@ -99,13 +116,14 @@ def draw_gal(center):
     # add floating equatorial axes on top of galactic axes
     gal_ax.axis["ra=%.2f" % ra] = gal_ax["fk5"].new_floating_axis(0, ra)
     gal_ax.axis["dec=%.2f" % dec] = gal_ax["fk5"].new_floating_axis(1, dec)
+    gal_ax.add_compass(loc=1)
 
     return gal_ax
 
 
-def draw_cel(center):
+def draw_cel(center, major_axes):
 
-    header = equ_header(center)
+    header = equ_header(center, major_axes)
 
     l,b=center.l(),center.b()
     cel_ax=pywcsgrid2.subplot(122, header=header)
@@ -117,6 +135,7 @@ def draw_cel(center):
     # add floating galacitc axes on top of equatorial axes
     cel_ax.axis["l=%.2f" % l] = cel_ax["gal"].new_floating_axis(0, l)
     cel_ax.axis["b=%.2f" % b] = cel_ax["gal"].new_floating_axis(1, b)
+    cel_ax.add_compass(loc=1)
 
     return cel_ax
 
@@ -132,16 +151,16 @@ def draw_ellipse(ax, center, region_string):
         p.set_zorder(10)
         ax.add_patch(p)
 
-def plot(center, region_string):
+def plot(center, major_axes, region_string):
     global gal_ax, cel_ax
 
 
-    fig = figure(None,(15,10))
+    fig = pylab.figure(None,(15,10))
     fig.subplots_adjust(wspace=0.4)
 
 
-    gal_ax=draw_gal(center)
-    cel_ax=draw_cel(center)
+    gal_ax=draw_gal(center, major_axes)
+    cel_ax=draw_cel(center, major_axes)
 
 
 
@@ -150,30 +169,52 @@ def plot(center, region_string):
     draw_ellipse(gal_ax, center, region_string)
 
 
-    suptitle('(l,b)=(%.2f,%.2f)\n(ra,dec)=(%.2f,%.2f)' % \
+    pylab.suptitle('(l,b)=(%.2f,%.2f)\n(ra,dec)=(%.2f,%.2f)' % \
              (center.l(),center.b(),center.ra(),center.dec()))
 
-    savefig('ellipse.png')
+    pylab.savefig('ellipse.png')
 
 
-#center=SkyDir(0.5,0.5,SkyDir.GALACTIC)
-center=SkyDir(-150.5,150.5,SkyDir.GALACTIC)
-center=SkyDir(-30,285,SkyDir.GALACTIC)
+def plot_galactic(center, major_axes, minor_axes, angle_galactic):
+    angle_equatorial = angle_equatorial_from_galactic(center, angle_galactic)
+    print 'equatorial angle',angle_equatorial 
 
-major_angle= 1
-minor_angle= 0.1
+    region_string="\n".join(
+        ["fk5; ellipse(%s, %s, %s, %s, %s) # color=red" % \
+         (center.ra(),center.dec(),minor_axes,major_axes,angle_equatorial),
+         "galactic; ellipse(%s, %s, %s, %s, %s) # color=blue dash=1" % \
+         (center.l(),center.b(),minor_axes,major_axes,angle_galactic)
+        ])
+    print region_string
 
-angle_galactic = 78.2
-angle_equatorial = angle_equatorial_from_galactic(center, angle_galactic)
-print 'angle',angle_equatorial 
+    plot(center, major_axes, region_string)
 
-region_string="\n".join(
-    ["fk5; ellipse(%s, %s, %s, %s, %s) # color=red" % \
-     (center.ra(),center.dec(),minor_angle,major_angle,angle_equatorial),
-     "galactic; ellipse(%s, %s, %s, %s, %s) # color=blue dash=1" % \
-     (center.l(),center.b(),minor_angle,major_angle,angle_galactic)
-    ])
+def test_around_poles():
 
-plot(center, region_string)
+    for center in [
+        # points around equatorial north pole
 
-print region_string
+        SkyDir(160,-10,SkyDir.GALACTIC),
+        SkyDir(80,-10,SkyDir.GALACTIC),
+        SkyDir(80,80,SkyDir.GALACTIC),
+        SkyDir(160,50,SkyDir.GALACTIC),
+
+        # points around equatorial south pole
+
+        SkyDir(280,-10,SkyDir.GALACTIC),
+        SkyDir(320,-10,SkyDir.GALACTIC),
+        SkyDir(320,-40,SkyDir.GALACTIC),
+        SkyDir(280,-40,SkyDir.GALACTIC),
+    ]:
+
+        major_axes= .2
+        minor_axes= 0.05
+        angle_galactic = 0
+
+        plot_galactic(center, major_axes, minor_axes, angle_galactic)
+        pylab.show()
+
+
+if __name__ == '__main__':
+
+    test_around_poles()
