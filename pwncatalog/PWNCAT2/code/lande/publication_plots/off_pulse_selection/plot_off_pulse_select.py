b63a059b4917854f7bf54fefb75e07d0e42068f4
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2012-01-24 22:50:00 +0000

    new file

---
+++ pwncatalog/PWNCAT2/code/lande/publication_plots/off_pulse_selection/plot_off_pulse_select.py
@@ -0,0 +1,10 @@
+
+cutoff_candidates = ['PSRJ0034-0534', 
+                     'PSRJ0633+1746', 
+                     'PSRJ1813-1246', 
+                     'PSRJ1836+5925', 
+                     'PSRJ2021+4026', 
+                     'PSRJ2055+2539', 
+                     'PSRJ2124-3358']
+
+pwn = cutoff_candidates[0]
