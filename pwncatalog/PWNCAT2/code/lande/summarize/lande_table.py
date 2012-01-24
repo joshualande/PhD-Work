25fd3a442f05eb5e7551e55bb96d0c0de931a319
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2012-01-24 21:26:58 +0000

    better order

---
+++ pwncatalog/PWNCAT2/code/lande/summarize/lande_table.py
@@ -0,0 +1,239 @@
+from os.path import join as j
+import StringIO
+from textwrap import dedent
+import shutil
+from lande_toolbag import OrderedDefaultdict
+import os.path
+import tempfile
+
+import yaml
+import asciitable
+
+fitdir='/nfs/slac/g/ki/ki03/lande/pwncatalog/PWNCAT2/analyze_psr/v6/analysis_no_plots/'
+savedir='/nfs/slac/g/ki/ki03/lande/pwncatalog/PWNCAT2/analyze_psr/v6/tables'
+
+if not os.path.exists(savedir): os.makedirs(savedir)
+
+def table_name(pwn):
+    pwn = pwn.replace('PSR','')
+    pwn = pwn.replace('-','$-$')
+    return pwn
+
+def write_latex(table, filebase, **kwargs):
+
+    outtable=StringIO.StringIO()
+
+    asciitable.write(table, outtable, 
+                     Writer=asciitable.AASTex,
+                     names=table.keys(),
+                     **kwargs
+                    )
+    t=outtable.getvalue()
+
+    lines = t.split('\n')
+    if lines[-1] == '': 
+        lines=lines[0:-1]
+
+    header = lines[0]
+    footer= lines[-1]
+
+    t = '\n'.join(lines[1:-1])
+
+
+    os.chdir(savedir)
+
+    open('%s.tex' % filebase,'w').write(t)
+
+    open('temp.tex','w').write(dedent(r"""
+        \documentclass{aastex}
+        \usepackage{amsmath}
+        \begin{document}
+        %s
+        \input{%s}
+        %s
+        \end{document}""" % (header,filebase,footer)))
+
+    os.system('pdflatex temp.tex')
+    shutil.move('temp.pdf','%s.pdf' % filebase)
+
+def get_results(pwn):
+    f = j(fitdir,pwn,'results_%s.yaml' % pwn)
+    if not os.path.exists(f): return None
+    results = yaml.load(open(f))
+
+    if not results.has_key('at_pulsar') or not results['at_pulsar'].has_key('gtlike'):
+        return None
+    return results
+
+def get_sed(pwn,binning,hypothesis):
+    filename=j(fitdir,pwn,'seds','sed_gtlike_%s_%s_%s.yaml' % (binning, hypothesis, pwn))
+    return yaml.load(open(filename))
+
+def all_energy_table(pwnlist):
+
+
+    table = OrderedDefaultdict(list)
+
+    flux_name=r'$F_{0.1-316}$'
+    ts_name='TS'
+    gamma_name=r'$\Gamma$'
+
+    for pwn in pwnlist:
+        print pwn,j(fitdir,pwn,'results_%s.yaml' % pwn)
+
+        table['PSR'].append(table_name(pwn))
+
+        results = get_results(pwn)
+
+        if results is None: 
+            table[ts_name].append('None')
+            table[flux_name].append('None')
+            table[gamma_name].append('None')
+        else:
+
+            pl=results['at_pulsar']['pointlike']
+            gt=results['at_pulsar']['gtlike']
+
+            ts=max(gt['TS'],0)
+            table[ts_name].append('%.1f' % ts)
+
+            if ts > 25:
+                flux=gt['flux']['flux']
+                flux_err=gt['flux']['flux_err']
+                table[flux_name].append('$%.2f \pm %.2f$' % (flux/1e-9,flux_err/1e-9) )
+            else:
+                if gt['upper_limit'] != -1:
+                    ul=gt['upper_limit']['flux']
+                    table[flux_name].append('$<%.2f$' % (ul/1e-9))
+                else:
+                    table[flux_name].append('None')
+
+
+            index=-1*gt['model']['Index']
+            index_err=-1*gt['model']['Index_err']
+
+            table[gamma_name].append('$%.2f \pm %.2f$' % (index,index_err) if ts > 25 else r'\nodata')
+
+    write_latex(table,
+                filebase='off_pulse_all_energy',
+                latexdict = dict(#caption=r'All Energy spectral fit for the %s LAT-detected Pulsars'  % len(pwnlist),
+                                 #preamble=r'\tabletypesize{\scriptsize}',
+                                 units={
+                                     flux_name:r'($10^{-9} \text{ph}\,\text{cm}^{-2}\,\text{s}^{-1}$)',
+                                 }))
+
+def each_energy_table(pwnlist):
+
+    table = OrderedDefaultdict(list)
+
+    TS1_name = '$TS_{0.1-1}$'
+    TS2_name = '$TS_{1-10}$'
+    TS3_name = '$TS_{10-316}$'
+
+    flux1_name = '$F_{0.1-1}$'
+    flux2_name = '$F_{1-10}$'
+    flux3_name = '$F_{10-316}$'
+
+    for pwn in pwnlist:
+
+        results = get_results(pwn)
+        table['PSR'].append(table_name(pwn))
+
+        if results is None or get_sed(pwn,'1bpd','at_pulsar') == {}: 
+            table[TS1_name].append('None')
+            table[flux1_name].append('None')
+
+            table[TS2_name].append('None')
+            table[flux2_name].append('None')
+
+            table[TS3_name].append('None')
+            table[flux3_name].append('None')
+        else:
+
+            sed = get_sed(pwn,'1bpd','at_pulsar')
+            ts = sed['Test_Statistic']
+            flux = sed['Ph_Flux']['Value']
+            flux_err = sed['Ph_Flux']['Error']
+            ul = sed['Ph_Flux']['Upper_Limit']
+
+            ts = [i if i > 0 else 0 for i in ts]
+
+            table[TS1_name].append('%.1f' % ts[0])
+            table[flux1_name].append('$%.2f \pm %.2f$' % (flux[0]/1e-9,flux_err[0]/1e-9) if ts[0] > 25 else '$<%.2f$' % (ul[0]/1e-9))
+
+            table[TS2_name].append('%.1f' % ts[1])
+            table[flux2_name].append('$%.2f \pm %.2f$' % (flux[1]/1e-9,flux_err[1]/1e-9) if ts[1] > 25 else '$<%.2f$' % (ul[1]/1e-9))
+
+            table[TS3_name].append('%.1f' % ts[2])
+            table[flux3_name].append('$%.2f \pm %.2f$' % (flux[2]/1e-9,flux_err[2]/1e-9) if ts[2] > 25 else '$<%.2f$' % (ul[2]/1e-9))
+
+    write_latex(table,
+                filebase='off_pulse_each_energy',
+                latexdict = dict(#caption=r'Energy bin spectral fit for the %s LAT-detected Pulsars'  % len(pwnlist),
+                                 #preamble=r'\tabletypesize{\scriptsize}',
+                                 units={
+                                     flux1_name:r'($10^{-9} \text{ph}\,\text{cm}^{-2}\,\text{s}^{-1}$)',
+                                     flux2_name:r'($10^{-9} \text{ph}\,\text{cm}^{-2}\,\text{s}^{-1}$)',
+                                     flux3_name:r'($10^{-9} \text{ph}\,\text{cm}^{-2}\,\text{s}^{-1}$)',
+                                 }))
+
+def cutoff_table(pwnlist,looppwn):
+
+    table = OrderedDefaultdict(list)
+
+    flux_name = r'$G_{0.1-316}$'
+    index_name = r'$\Gamma$'
+    cutoff_name = r'$E_\text{cutoff}$'
+    ts_cutoff_name = r'$\text{TS}_\text{cutoff}$'
+
+    for pwn in looppwn:
+        results = get_results(pwn)
+        if results is None: continue
+
+        table['PSR'].append(table_name(pwn))
+
+        cutoff=results['at_pulsar']['gtlike']['test_cutoff']
+
+        if cutoff != -1:
+
+            flux=cutoff['flux_1']['eflux']
+            flux_err=cutoff['flux_1']['eflux_err']
+            table[flux_name].append('$%.2f \pm %.2f$' % (flux/1e-12,flux_err/1e-12))
+            index=-1*cutoff['model_1']['Index1']
+            index_err=cutoff['model_1']['Index1_err']
+            table[index_name].append('$%.2f \pm %.2f$' % (index,index_err))
+            cutoff_energy=cutoff['model_1']['Cutoff']
+            cutoff_energy_err=cutoff['model_1']['Cutoff_err']
+            table[cutoff_name].append('$%.2f \pm %.2f$' % (cutoff_energy/1000,cutoff_energy_err/1000))
+            ts_cutoff = max(cutoff['TS_cutoff'],0)
+            table[ts_cutoff_name].append('%.1f' % ts_cutoff)
+        else:
+            table[flux_name].append('None')
+            table[index_name].append('None')
+            table[cutoff_name].append('None')
+            table[ts_cutoff_name].append('None')
+
+    write_latex(table,
+                filebase='off_pulse_cutoff_test',
+                latexdict = dict(#caption=r'Spectral fitting of pulsar wind nebula candidates with low energy component.',
+                                 #col_align=r'lrrrr',
+                                 #preamble=r'\tabletypesize{\scriptsize}',
+                                 units={
+                                     flux_name:r'($10^{-12}$\,erg\,cm$^{-2}$\,s$^{-1}$)',
+                                     cutoff_name:r'(GeV)',
+                                 }))
+
+
+cutoff_candidates = ['PSRJ0034-0534', 
+                     'PSRJ0633+1746', 
+                     'PSRJ1813-1246', 
+                     'PSRJ1836+5925', 
+                     'PSRJ2021+4026', 
+                     'PSRJ2055+2539', 
+                     'PSRJ2124-3358']
+
+pwnlist=sorted(yaml.load(open('../pwndata/pwncat2_data_lande.yaml')).keys())
+all_energy_table(pwnlist)
+each_energy_table(pwnlist)
+cutoff_table(pwnlist, looppwn=cutoff_candidates)
+
---
+++ pwncatalog/PWNCAT2/code/lande/summarize/lande_website.py
@@ -0,0 +1,243 @@
+import sys
+from glob import glob
+import yaml
+from StringIO import StringIO
+import re
+import os
+from os.path import join,exists
+from os.path import join as j
+from collections import defaultdict
+
+import asciitable
+
+from lande_toolbag import OrderedDefaultdict
+
+base='/nfs/slac/g/ki/ki03/lande/pwncatalog/PWNCAT2/analyze_psr/v6/'; at_pulsar='at_pulsar'
+
+analysis='analysis_plots'
+#analysis='analysis'
+pwnlist=sorted([os.path.basename(i) for i in glob(j(base,analysis,'*')) if os.path.isdir(i)])
+
+website=join(base,'website')
+
+if not os.path.exists(website): os.makedirs(website)
+
+
+
+def get_results(pwn):
+    f = j(base,analysis.replace('plots','no_plots'),pwn,'results_%s.yaml' % pwn)
+    #f = j(base,analysis,pwn,'results_%s.yaml' % pwn)
+
+    if not os.path.exists(f): return None
+    results = yaml.load(open(f))
+
+    #for hypothesis in [at_pulsar, 'point', 'extended']:
+    for hypothesis in [at_pulsar]:
+        h=results[hypothesis]
+        if not results.has_key(hypothesis):
+            results[hypothesis]=defaultdict(lambda:-1),
+        for t in ['gtlike','pointlike']:
+            if not h.has_key(t):
+                h[t]=defaultdict(lambda:-1)
+            for i in ['model', 'upper_limit', 'flux']:
+                if not h[t].has_key(i):
+                    h[t][i]=defaultdict(lambda:-1)
+    return results
+
+def get_sed(pwn,binning,hypothesis):
+    filename=j(base,analysis.replace('plots','no_plots'),pwn,'seds','sed_gtlike_%s_%s_%s.yaml' % (binning, hypothesis, pwn))
+    if os.path.exists(filename) and yaml.load(open(filename)) != {}:
+        return yaml.load(open(filename))
+    elif binning == '1bpd':
+        d=defaultdict(lambda: defaultdict(lambda:[-1,-1,-1]))
+        d['Test_Statistic']=[-1,-1,-1]
+        return d
+    else:
+        raise Exception("...")
+
+def get_t2t_table(table, **kwargs):
+
+    outtable=StringIO()
+
+    asciitable.write(table, outtable, 
+                     Writer=asciitable.FixedWidth,
+                     names=table.keys(),
+                     **kwargs
+                    )
+    t=outtable.getvalue()
+
+    # this is required by t2t for tables
+    # see for exmaple: http://txt2tags.org/markup.html
+    t='||' + t[2:]
+    return t
+
+
+def t2t(lines,name): 
+    """ create the HTML for a given t2t file. """
+    filename = join(website,'%s.t2t' % name)
+    
+    temp=open(filename,'w')
+    temp.write(
+        '\n'.join(lines))
+    temp.close()
+        
+    os.system('txt2tags --target html --style color.css --css-sugar %s' % filename)
+
+
+def format_table(pwnlist):
+
+
+    table = OrderedDefaultdict(list)
+
+    flux_name=r'F(0.1-316)'
+    gamma_name=r'Gamma'
+
+    for pwn in pwnlist:
+        print pwn
+
+        results = get_results(pwn)
+        if results is None: continue
+
+        gt=results[at_pulsar]['gtlike']
+
+
+        ts=max(gt['TS'],0)
+        
+        try:
+            ts_ext=max(results['extended']['gtlike']['ts_ext'],0)
+        except:
+            ts_ext=-1
+
+
+        if ts > 25:
+            flux=gt['flux']['flux']
+            flux_err=gt['flux']['flux_err']
+        else:
+            try:
+                ul=gt['upper_limit']['flux']
+            except:
+                ul=-1
+
+
+        index=-1*gt['model']['Index']
+        index_err=-1*gt['model']['Index_err']
+
+
+        sed = get_sed(pwn,'1bpd',at_pulsar)
+        bandts = map(lambda x: max(x,0), sed['Test_Statistic'])
+        bandflux = sed['Ph_Flux']['Value']
+        bandflux_err = sed['Ph_Flux']['Error']
+        bandul = sed['Ph_Flux']['Upper_Limit']
+
+
+        table['PSR'].append('[%s %s.html]' % (pwn,pwn))
+        table['TS'].append('%.1f' % ts)
+        table['TS_ext'].append('%.1f' % ts_ext)
+
+        table[flux_name].append('%.2f +/- %.2f' % (flux/1e-9,flux_err/1e-9) if ts>=25 else '<%.2f' % (ul/1e-9))
+
+        table[gamma_name].append('%.2f +/- %.2f' % (index,index_err) if ts>=25 else '-')
+
+        table['TS(.1-1)'].append('%.1f' % bandts[0])
+        table['Flux(.1-1)'].append('%.2f +/- %.2f' % (bandflux[0]/1e-9,bandflux_err[0]/1e-9) if bandts[0] > 25 else '<%.2f' % (bandul[0]/1e-9))
+
+        table['TS(1-10)'].append('%.1f' % bandts[1])
+        table['Flux(1-10)'].append('%.2f +/- %.2f' % (bandflux[1]/1e-9,bandflux_err[1]/1e-9) if bandts[1] > 25 else '<%.2f' % (bandul[1]/1e-9))
+
+        table['TS(10-316)'].append('%.1f' % bandts[2])
+        table['Flux(10-316)'].append('%.2f +/- %.2f' % (bandflux[2]/1e-9,bandflux_err[2]/1e-9) if bandts[2] > 25 else '<%.2f' % (bandul[2]/1e-9))
+
+    return get_t2t_table(table)
+
+def build_main_website():
+
+    index_t2t = []
+    index_t2t.append('PWNCatalog+\n\n')
+    index_t2t.append(format_table(pwnlist))
+    t2t(index_t2t, 'index')
+
+def build_each_page(pwn):
+    results = get_results(pwn)
+    if results is None: return
+
+    index_t2t = []
+    index_t2t.append(pwn+'\n\n')
+    index_t2t.append(format_table([pwn]))
+    index_t2t.append('')
+    index_t2t.append('[Analysis Folder ../%s/%s]' % (analysis,pwn))
+    index_t2t.append('')
+    index_t2t.append('[log ../%s/%s/log_%s.txt]' % (analysis,pwn,pwn))
+
+    hypothesis=at_pulsar
+
+    get_img_table = lambda *args: index_t2t.append('|| ' + ' | '.join(['[../%s/%s/%s]' % (analysis,pwn,i) for i in args]) + ' |\n\n')
+
+    title = lambda i: index_t2t.append('== %s ==' % i)
+
+    title('Phase Info')
+    get_img_table(
+        'plots/phaseogram_%s.png' % (pwn), 
+        'plots/phase_vs_time_%s.png' % (pwn))
+            
+    title('TS Maps')
+    get_img_table(
+        'plots/tsmap_source_%s_%s.png' % (hypothesis,pwn),
+        'plots/tsmap_residual_%s_%s.png' % (hypothesis,pwn))
+
+    title('Smoothed Counts (0.1)')
+    get_img_table(
+        'plots/source_0.1_%s_%s.png' % (hypothesis,pwn),
+        'plots/sources_0.1_%s_%s.png' % (hypothesis,pwn))
+
+    title('Counts (0.1)')
+    get_img_table(
+        'plots/counts_residual_0.1_%s_%s.png' % (hypothesis,pwn),
+        'plots/counts_source_0.1_%s_%s.png' % (hypothesis,pwn))
+
+    title('Band TS Maps')
+    get_img_table(
+        'plots/band_tsmap_source_%s_%s.png' % (hypothesis,pwn),
+        'plots/band_tsmap_residual_%s_%s.png' % (hypothesis,pwn))
+
+
+    title('Band Smoothed Counts (0.1)')
+    get_img_table(
+        'plots/band_source_0.1_%s_%s.png' % (hypothesis,pwn),
+        'plots/band_sources_0.1_%s_%s.png' % (hypothesis,pwn))
+
+    get_img_table(
+        'seds/sed_gtlike_4bpd_%s_%s.png' % (hypothesis,pwn),
+        'seds/sed_gtlike_2bpd_%s_%s.png' % (hypothesis,pwn),
+        'seds/sed_gtlike_1bpd_%s_%s.png' % (hypothesis,pwn),
+        'seds/sed_gtlike_%s_%s.png' % (hypothesis,pwn),
+        'seds/sed_pointlike_%s_%s.png' % (hypothesis,pwn))
+
+    get_img_table(
+        'plots/test_cutoff_%s_%s.png' % (hypothesis,pwn))
+
+
+    title('Extra: Smoothed Counts (0.25)')
+    get_img_table(
+        'plots/source_0.25_%s_%s.png' % (hypothesis,pwn),
+        'plots/sources_0.25_%s_%s.png' % (hypothesis,pwn))
+
+    title('Extra: Counts (0.25)')
+    get_img_table(
+        'plots/counts_source_0.25_%s_%s.png' % (hypothesis,pwn),
+        'plots/counts_residual_0.25_%s_%s.png' % (hypothesis,pwn))
+
+    title('Extra: Band Smoothed Counts (0.25)')
+    get_img_table(
+        'plots/band_source_0.25_%s_%s.png' % (hypothesis,pwn),
+        'plots/band_sources_0.25_%s_%s.png' % (hypothesis,pwn))
+    index_t2t.append("""```
+%s
+```""" % yaml.dump(results))
+
+    t2t(index_t2t, pwn)
+
+def build_all_pages():
+    for pwn in pwnlist: build_each_page(pwn)
+
+build_main_website()
+build_all_pages()
