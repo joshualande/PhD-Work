dabe0b9233b17b17e5b20d462561c26adbec2e54
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2012-02-03 23:00:13 +0000

    allow scaling factor

--- pointlike/lande_diffuse.py
+++ pointlike/lande_diffuse.py
@@ -12,7 +12,8 @@ class ApproximateIsotropic(DiffuseSource):
                  diffuse_sources,
                  file,
                  emin, emax,
-                 skydir):
+                 skydir,
+                 scaling_factor=1):
         """ Approximates a linear combination of
             diffuse sources as a single isotropic
             spectrum. 
@@ -22,28 +23,29 @@ class ApproximateIsotropic(DiffuseSource):
 
         self.name = name
         self.smodel = PowerLaw(p=[1,1],index_offset=1)
+        self.scaling_factor
 
         for ds in diffuse_sources:
             if len(ds.dmodel) != 1: raise Exception("dmodels must have length 1")
 
         self.file  = open(file,'w')
         self.file.write(
-            self._make_file(diffuse_sources, skydir, emin, emax)
+            self._make_file(diffuse_sources, skydir, emin, emax, self.scaling_factor)
             )
         self.file.close()
 
         self.dmodel = [IsotropicSpectrum(file)]
 
     @staticmethod
-    def get_dnde(diffuse_sources, skydir, energy):
-        return sum(ds.smodel(energy)*ds.dmodel[0](skydir, energy) for ds in diffuse_sources)
+    def get_dnde(diffuse_sources, skydir, energy, scaling_factor):
+        return scaling_factor*sum(ds.smodel(energy)*ds.dmodel[0](skydir, energy) for ds in diffuse_sources)
 
     @staticmethod
-    def _make_file(diffuse_sources, skydir, emin, emax, npts=1e3):
+    def _make_file(diffuse_sources, skydir, emin, emax, scaling_factor, npts=1e3):
 
         energies = np.logspace(np.log10(emin), np.log10(emax), npts)
 
-        fluxes = [ ApproximateIsotropic.get_dnde(diffuse_sources, skydir, energy) for energy in energies ]
+        fluxes = [ ApproximateIsotropic.get_dnde(diffuse_sources, skydir, energy, scaling_factor) for energy in energies ]
 
         return '\n'.join(['%s\t%s' % (e,f) for e,f in zip(energies, fluxes)])
 
