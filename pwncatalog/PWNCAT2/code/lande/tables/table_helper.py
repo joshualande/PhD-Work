c594a3a3652323188cc9f57b824fcc323d242dd4
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2012-02-10 18:21:29 +0000

    update

--- pwncatalog/PWNCAT2/code/lande/tables/table_helper.py
+++ pwncatalog/PWNCAT2/code/lande/tables/table_helper.py
@@ -8,7 +8,7 @@ import os.path
 import yaml
 import asciitable
 
-base='$pwndata/spectral/v11/'
+base='$pwndata/spectral/v12/'
 
 fitdir=expandvars(j(base,'analysis_no_plots/'))
 savedir=expandvars(j(base,'tables'))
