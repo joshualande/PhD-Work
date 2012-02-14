3193856eb4550a64905127f5067f29412463b3f4
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2012-02-14 16:55:26 +0000

    better modifications

--- pwncatalog/PWNCAT2/code/modify/modify_psr_lande.py
+++ pwncatalog/PWNCAT2/code/modify/modify_psr_lande.py
@@ -6,6 +6,8 @@ import modify_psr_base
 from skymaps import SkyDir
 from uw.like.pointspec_helpers import PointSource
 from uw.like.Models import PowerLaw
+from uw.like.SpatialModels import Disk
+from uw.like.roi_extended import ExtendedSource
 
 def modify_roi(name,roi):
     modify_psr_base.modify_roi(name,roi)
@@ -27,14 +29,21 @@ def modify_roi(name,roi):
         # I foudn that this source had large residual surrounding it. So always fit it
         roi.modify(which='2FGL J1044.5-5737', free=True)
 
-        # Add new source 'seed' that represents residual found in the TSmap at the top left
+        # Add new source 'seed1' that represents residual found in the TSmap at the top left
         # (in galactic coordinates) at (l,b)=(286.252,0.522)
         # The best fit parameters of this new source are taken from
         # $pwnpersonal/individual_sources/PSRJ1023-5746/v1/iteration_v1/run.py
-        ps = PointSource(name='seed',
+        ps1 = PointSource(name='seed1',
                          skydir=SkyDir(286.2517892658979,0.5217112872908655,SkyDir.GALACTIC),
                          model=PowerLaw(norm=3.8835219935993907e-12,index=2.2310422346640504))
-        roi.add_source(ps)
+        roi.add_source(ps1)
+
+        # Add new source 'seed2' that represents additional residual found from
+        # $pwnpersonal/individual_sources/PSRJ1023-5746/v2/iteration_v1/run.py
+        ps2 = PointSource(name='seed2',
+                         skydir=SkyDir(284.5071255898265,0.9734675040757886,SkyDir.GALACTIC),
+                         model=PowerLaw(norm=2.787037074673656e-12,index=2.2750149645582809))
+        roi.add_source(ps2)
         
 
     if name == 'PSRJ1420-6048':
@@ -82,6 +91,15 @@ def modify_roi(name,roi):
         # I found that this source was badly misfit, so set free
         roi.modify(which='2FGL J1747.1-3000', free=True)
 
+        # In previous analysis 
+        # $pwnpersonal/individual_sources/PSRJ1746-3239/v1/iteration_v1/run.py
+        # I found the need for a new source
+        ps = PointSource(name='seed1',
+                         skydir=SkyDir(357.2349498889614,-1.0448205537267325,SkyDir.GALACTIC),
+                         model=PowerLaw(norm=3.2270327105385296e-12,
+                                        index=2.1855137088271537))
+        roi.add_source(ps)
+
     if name == 'PSRJ1747-2958':
 
         # In previous analysis 
@@ -93,19 +111,23 @@ def modify_roi(name,roi):
         roi.modify(which='2FGL J1746.6-2851c', free=True)
         roi.modify(which='2FGL J1747.3-2825c', free=True)
 
+        # This further away source is also not fit well. See
+        # $pwndata/spectral/v10/analysis_plots/PSRJ1747-2958/plots/source_0.1_extended_PSRJ1747-2958.png
+        roi.modify(which='2FGL J1732.5-3131', free=True)
+
         # After freeing those sources, I found a residual emission, which
         # I will model as a new source w/ arameters taken from
         # See $pwnpersonal/individual_sources/PSRJ1747-2958/v1/iteration_v1/run.py
-        ps = PointSource(name='seed',
+        ps = PointSource(name='seed1',
                          skydir=SkyDir(265.609,-28.654),
                          model=PowerLaw(norm=4.54e-12,
                                         index=2.20))
         roi.add_source(ps)
-        roi.print_summary()
 
 
     if name == 'PSRJ2032+4127':
 
+        """
         # In previous analysis 
         # $pwndata/spectral/v9/analysis_no_plots/PSRJ2032+4127/results_PSRJ2032+4127.yaml
         # I foudn that these 2 nearby sources (correlated with the Gamma Cygni SNR + Pulsar)
@@ -114,5 +136,33 @@ def modify_roi(name,roi):
 
         roi.modify(which='2FGL J2019.1+4040', free=True)
         roi.modify(which='2FGL J2021.5+4026', free=True)
+        """
+
+        # In previous analysis 
+        # $pwndata/spectral/v10/analysis_no_plots/PSRJ2032+4127/results_PSRJ2032+4127.yaml
+        # I found that just freeing the two sources near gamma 
+        # cygni was not sufficient. A better solution
+        # is to explicitly add in Gamma Cygni as an SNR
+
+
+        # delete source associated with Gamma Cygni SNR
+        roi.del_source(which='2FGL J2019.1+4040')
+
+        # Free the pulsar component
+        roi.modify(which='2FGL J2021.5+4026', free=True)
+
+        # Add in Gamma Cygni SNR using best fit parmaeters
+        # From extended soruce search paper
+        model = PowerLaw(index=2.42)
+        model.set_flux(2e-9,1e4,1e5)
+        es=ExtendedSource(name='Gamma Cygni',
+                          model=model,
+                          spatial_model=Disk(l=78.24,b=2.20,sigma=0.63))
+        roi.add_source(es)
+
+    if name == 'PSRJ2021+4026':
+        # Remove this nearby source which is associated with the
+        # Gamma-Cygni SNR
+        roi.del_source(which='2FGL J2019.1+4040')
 
 
