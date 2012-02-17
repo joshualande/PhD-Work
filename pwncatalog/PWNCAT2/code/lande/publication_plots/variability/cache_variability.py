a1b8a09cad38f9c28f8d4d50336027d17a7e8c56
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2012-02-17 18:05:57 +0000

    update codes

--- pwncatalog/PWNCAT2/code/lande/publication_plots/variability/cache_variability.py
+++ pwncatalog/PWNCAT2/code/lande/publication_plots/variability/cache_variability.py
@@ -6,9 +6,10 @@ from os.path import expandvars, join, exists
 
 pwnlist = yaml.load(open(expandvars('$pwncode/pwndata/pwncat2_data_lande.yaml')))
 
-def get_ts_var():
-    folder = '/nfs/slac/g/ki/ki03/lande/pwncatalog/PWNCAT2/analyze_psr/spectral/v10/variability/v3'
+folder = '/nfs/slac/g/ki/ki03/lande/pwncatalog/PWNCAT2/analyze_psr/spectral/v13/variability/v1'
 
+def get_ts():
+    ts_point = []
     ts_var = []
     for pwn in pwnlist.keys():
 
@@ -19,31 +20,16 @@ def get_ts_var():
         f=yaml.load(open(results))
 
         ts_var.append(f['TS_var']['gtlike'])
+        ts_point.append(f['all_time']['gtlike']['TS'])
 
-    return ts_var
+    return ts_point,ts_var
 
-#def get_ts_point():
-#    folder = '/nfs/slac/g/ki/ki03/lande/pwncatalog/PWNCAT2/analyze_psr/spectral/v10/analysis_plots/'
-#
-#    ts_point = []
-#    for pwn in pwnlist.keys():
-#
-#        results = join(folder,pwn,'results_%s.yaml' % pwn)
-#
-#        assert exists(results)
-#
-#        f=yaml.load(open(results))
-#        print pwn,f['at_pulsar']['gtlike'],f['at_pulsar']['gtlike']['TS']
-#
-#        ts_point.append(f['at_pulsar']['gtlike']['TS'])
-#
 
-ts_var = get_ts_var()
-#ts_point = get_ts_point()
+ts_point,ts_var = get_ts()
 
 d = dict(
-#    ts_point=ts_point
-    ts_var=ts_var
+    ts_point=ts_point,
+    ts_var=ts_var,
     )
 
 open('ts_var.yaml','w').write(
