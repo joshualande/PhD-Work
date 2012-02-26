36992c8e5126b3093b3b1f923f11e21b60f27c4b
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2012-02-26 04:23:55 +0000

    make more general

--- pointlike/lande_pulsar.py
+++ pointlike/lande_pulsar.py
@@ -1,41 +1,43 @@
+from os.path import expandvars
+
 import pylab as P
 import numpy as np
 
 from uw.utilities.fitstools import rad_extract
-
-from uw.pulsar.lc_plotting_func import PulsarLightCurve
 from uw.pulsar.phase_range import PhaseRange
-def _get_pulsar_data(ft1, radius=1, emin=100, emax=300000):
 
-    plc = PulsarLightCurve(ft1, emin=emin, emax=emax, radius=radius)
-    plc.fill_phaseogram()
-    phases = plc.get_phases()
-    times = plc.get_times()
-    return phases, times
-
-def plot_phaseogram(name, ft1, filename, title=None, off_pulse=None):
+def plot_phaseogram(ft1, nbins=100, filename=None, title=None, off_pulse=None, axes=None, **kwargs):
     """ Simple code to plot a phaseogram. """
-    phases, times = _get_pulsar_data(ft1)
+    phases = get_phases(ft1, **kwargs)
 
-    nbins=50
-    fig = P.figure(None, figsize=(5,5))
-    axes = fig.add_subplot(111)
-    axes.hist(phases,bins=np.linspace(0,1,nbins+1),histtype='step',ec='red',normed=True,lw=1)
+    if axes is None:
+
+        fig = P.figure(None, figsize=(5,5))
+        axes = fig.add_subplot(111)
+
+    bins = np.linspace(0,1,nbins+1)
+    axes.hist(phases,bins=bins,histtype='step',ec='red',normed=True,lw=1)
     axes.set_xlim(0,1)
 
-    if title is None: title = 'Phaseogram for %s' % name
-    axes.set_title(title)
+    if title is not None: 
+        axes.set_title(title)
 
-    axes.set_xlabel('phase')
+    axes.set_xlabel('Phase')
+    axes.set_ylabel('Counts')
+
+    axes.grid=True
 
     if off_pulse is not None:
         PhaseRange(off_pulse).axvspan(axes=axes, alpha=0.5, color='blue')
-    P.savefig(filename)
-    return axes
 
-def plot_phase_vs_time(name, ft1, filename, title=None, off_pulse=None):
+    if filename is not None:
+        P.savefig(filename)
+
+    return axes, bins
+
+def plot_phase_vs_time(ft1, filename, title=None, off_pulse=None, **kwargs):
     """ Simple code to plot phase vs time. """
-    phases, times = _get_pulsar_data(ft1)
+    phases, times = get_phases_and_times(ft1, **kwargs)
 
     # here, put a 2d histogram
     fig = P.figure(None, figsize=(5,5))
@@ -54,18 +56,31 @@ def plot_phase_vs_time(name, ft1, filename, title=None, off_pulse=None):
     axes.set_xlabel('phase')
     axes.set_ylabel('MJD')
 
-    if title is None: title = 'Phase vs Time for %s' % name
-    axes.set_title(title)
+    if title is not None: 
+        title = 'Phase vs Time for %s' % name
+        axes.set_title(title)
 
     P.savefig(filename)
     return axes
 
-def get_phases(ft1, skydir, emin, emax, radius):
-    ed = rad_extract(ft1,skydir,radius,return_cols=['PULSE_PHASE'])
-    all_phases = ed['PULSE_PHASE']
+from uw.like.roi_image import memoize
+@memoize
+def get_all_phases(ft1, skydir):
+    """ Cache photons = faster """
+    ed = rad_extract(expandvars(ft1),skydir,radius_function=180,return_cols=['PULSE_PHASE', 'TIME'])
+    return ed
 
-    cut = (ed['ENERGY'] < emax) & (ed['DIFFERENCES'] < np.radians(radius))
-    return all_phases[cut]
+
+def get_phases_and_times(ft1, skydir, emin, emax, radius):
+    ed = get_all_phases(ft1, skydir)
+    all_phases = ed['PULSE_PHASE']
+    all_times = ed['TIME']
+
+    cut = (ed['ENERGY'] >= emin) & (ed['ENERGY'] <= emax) & (ed['DIFFERENCES'] <= np.radians(radius))
+    return all_phases[cut], all_times[cut]
+
+def get_phases(*args, **kwargs):
+    return get_phases_and_times(*args, **kwargs)[0]
 
 
 
