1297756a41e283450874ae0be54c56e591ea0734
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2012-03-04 02:50:22 +0000

    upate

--- fermi/monte_carlo/diffuse_testing/spectral_fitting/loop.py
+++ fermi/monte_carlo/diffuse_testing/spectral_fitting/loop.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python
+from os.path import join, expandvars, exists
+from os import makedirs
+
+# version 1
+num=100
+savedir = expandvars('$fitdiffdata/v1')
+
+for i in range(num):
+    istr='%05d' % i
+
+    jobdir = join(savedir,istr)
+    if not exists(jobdir): makedirs(jobdir)
+
+    run = join(jobdir,'run.sh')
+    open(run,'w').write("""python $fitdiffcode/simulate.py %g""" % i)
+
+submit_all = join(savedir,'submit_all.sh')
+open(submit_all,'w').write("submit_all */run.sh $@")
+
