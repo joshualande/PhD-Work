79fa74fd486b782c8633c02b1f8d40ad62e9119d
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2012-03-14 03:07:56 +0000

    this code is ugly

--- fermi/monte_carlo/test_mapcube_cutting/fitdiff/merge.py
+++ fermi/monte_carlo/test_mapcube_cutting/fitdiff/merge.py
@@ -1,5 +1,5 @@
 from os.path import join, expandvars
-from glob import iglob
+from glob import glob
 from collections import defaultdict
 import numpy as np
 
@@ -8,15 +8,16 @@ import yaml
 
 from uw.utilities.makerec import makefits
 
-savedir = expandvars(join('$fitdiffdata','v1'))
+savedir = expandvars(join('$fitdiffdata','v5'))
 
 
 results = defaultdict(list)
 
-all_results=iglob(join(savedir,'*','*','results_*.yaml'))
+all_results=glob(join(savedir,'*','*','results_*.yaml'))
+s=len(all_results)
 
 for i,r in enumerate(all_results):
-    if i % 10==0: print i
+    if i % 10==0: print '%s/%s' % (i,s)
 
     x = yaml.load(open(r))
 
@@ -28,32 +29,45 @@ for i,r in enumerate(all_results):
     results['emin'].append(x['emin'])
     results['emax'].append(x['emax'])
 
-    diff = x['diffuse']
-    if diff.has_key('Isotropic Diffuse (isotrop_2year_P76_source_v0.txt)') and \
-       diff.has_key('Galactic Diffuse (ring_2year_P76_v0.fits)'):
-        gal=diff['Galactic Diffuse (ring_2year_P76_v0.fits)']
-        iso=diff['Isotropic Diffuse (isotrop_2year_P76_source_v0.txt)']
+    p = x['pointlike']
+    g = x['gtlike']
+
+    diff = p['diffuse']
+
+    if diff.has_key('isotrop_2year_P76_source_v0.txt') and \
+       diff.has_key('ring_2year_P76_v0.fits'):
+
+        results['pointlike_norm'].append(p['diffuse']['ring_2year_P76_v0.fits']['Scale'])
+        results['pointlike_norm_err'].append(p['diffuse']['ring_2year_P76_v0.fits']['Scale_err'])
+        results['pointlike_norm_mc'].append(p['mc']['ring_2year_P76_v0.fits']['Scale'])
+
+        results['gtlike_norm'].append(g['diffuse']['ring_2year_P76_v0.fits']['Value'])
+        results['gtlike_norm_err'].append(g['diffuse']['ring_2year_P76_v0.fits']['Value_err'])
+        results['gtlike_norm_mc'].append(g['mc']['ring_2year_P76_v0.fits']['Value'])
 
     elif diff.has_key('isotrop_2year_P76_source_v0.txt'):
-        iso=diff['isotrop_2year_P76_source_v0.txt']
-        gal=None
+
+        results['pointlike_norm'].append(p['diffuse']['isotrop_2year_P76_source_v0.txt']['Scale'])
+        results['pointlike_norm_err'].append(p['diffuse']['isotrop_2year_P76_source_v0.txt']['Scale_err'])
+        results['pointlike_norm_mc'].append(p['mc']['isotrop_2year_P76_source_v0.txt']['Scale'])
+
+        results['gtlike_norm'].append(g['diffuse']['isotrop_2year_P76_source_v0.txt']['Normalization'])
+        results['gtlike_norm_err'].append(g['diffuse']['isotrop_2year_P76_source_v0.txt']['Normalization_err'])
+        results['gtlike_norm_mc'].append(g['mc']['isotrop_2year_P76_source_v0.txt']['Normalization'])
 
     elif diff.has_key('Sreekumar Isotropic'):
-        iso = diff['Sreekumar Isotropic']
-        gal = None
+        results['pointlike_norm'].append(p['diffuse']['Sreekumar Isotropic']['Norm'])
+        results['pointlike_norm_err'].append(p['diffuse']['Sreekumar Isotropic']['Norm_err'])
+        results['pointlike_norm_mc'].append(p['mc']['Sreekumar Isotropic']['Norm'])
+
+        results['gtlike_norm'].append(g['diffuse']['Sreekumar Isotropic']['Prefactor'])
+        results['gtlike_norm_err'].append(g['diffuse']['Sreekumar Isotropic']['Prefactor_err'])
+        results['gtlike_norm_mc'].append(g['mc']['Sreekumar Isotropic']['Prefactor'])
+
     else:
         print x
         raise Exception("...")
 
-
-    results['galnorm'].append(gal['Norm'] if gal is not None else np.nan)
-    results['galnorm_err'].append(gal['Norm_err'] if gal is not None else np.nan)
-
-    results['galindex'].append(gal['Index'] if gal is not None else np.nan)
-    results['galindex_err'].append(gal['Index_err'] if gal is not None else np.nan)
-
-    results['isonorm'].append(iso['Scale'])
-    results['isonorm_err'].append(iso['Scale_err'])
 
 
 rec = fromarrays(results.values(), names=results.keys())
