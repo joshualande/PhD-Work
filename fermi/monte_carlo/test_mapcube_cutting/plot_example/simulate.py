29af7db9a1a1ceb17c8a41131855c887eecf5763
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2012-03-14 02:02:51 +0000

    cleanup

--- fermi/monte_carlo/test_mapcube_cutting/plot_example/simulate.py
+++ fermi/monte_carlo/test_mapcube_cutting/plot_example/simulate.py
@@ -1,77 +1,50 @@
 #!/usr/bin/env python
 
-from argparse import ArgumentParser
-import shutil
-from tempfile import mkdtemp
 from os.path import join
-
-import yaml
-import numpy as np
 
 from skymaps import SkyDir
 
-from uw.like.Models import Constant
-from uw.like.roi_monte_carlo import SpectralAnalysisMC
-from uw.like.pointspec import DataSpecification
-
+from uw.like.roi_monte_carlo import MonteCarlo
+from uw.like.pointspec import DataSpecification, SpectralAnalysis
+from uw.like.pointspec_helpers import get_default_diffuse
 
 from lande.fermi.data.catalogs import get_2fgl
-from lande.fermi.likelihood.diffuse import get_background, get_sreekumar
 from lande.fermi.likelihood.save import diffusedict, skydirdict
 from lande.utilities.random import random_on_sphere
 from lande.utilities.tools import savedict
 
 
 
-np.random.seed(0)
+for roi_dir in [SkyDir(0,0, SkyDir.GALACTIC), SkyDir(70,85, SkyDir.GALACTIC)]:
+    
+    savedir='datadir_l_%d_b_%d' % (roi_dir.l(), roi_dir.b())
 
-emin=1e2
-emax=1e5
+    emin=1e3
+    emax=1e5
 
-diffdir='/afs/slac/g/glast/groups/diffuse/rings/2year/'
+    diffdir='/afs/slac/g/glast/groups/diffuse/rings/2year/'
 
-diffuse = get_dfeault_diffuse
-iso = get_background(join(diffdir,'isotrop_2year_P76_source_v0.txt'))
-ring = get_background(join(diffdir,'ring_2year_P76_v0.fits'))
-sreekumar = get_sreekumar(diff_factor=1)
-
-catdict = get_2fgl()
-ft2 = catdict['ft2'],
-ltcube = catdict['ltcube'],
-
-roi_dir = SkyDir(0,0, SkyDir.GALACTIC)
-
-ft1='ft1.fits',
-m=MonteCarlo(
-    emin = emin,
-    emax = emax,
-    irf='P7SOURCE_V6',
-    roi_dir=roi_dir,
-    maxROI=10,
-    seed=0,
-    ft1=ft1,
-    ft2=ft2,
-    gtifile = ltcube,
-    savedir='data'
-    )
-
-ds = DataSpecification(
-    ft1files = join(tempdir,'ft1.fits'),
-    ft2files = ft2,
-    ltcube = ltcube,
-    binfile = join(tempdir,'binned.fits'))
+    diffuse_sources = get_default_diffuse(diffdir=diffdir, 
+                                          gfile='ring_2year_P76_v0.fits',
+                                          ifile='isotrop_2year_P76_source_v0.txt')
 
 
-sa = SpectralAnalysis(ds,
-    emin = emin,
-    emax = emax,
-    irf='P7SOURCE_V6',
-    roi_dir=roi_dir,
-    minROI=10,
-    maxROI=10,
-)
+    catdict = get_2fgl()
+    ft2 = catdict['ft2']
+    ltcube = catdict['ltcube']
 
-roi = sa.roi(roi_dir=roi_dir, diffuse_sources = diffuse_sources)
 
-roi.plot_counts_map(filename='counts_map_%s.png'  % istr, **plot_kwargs)
-
+    ft1 = join(savedir,'ft1.fits')
+    m=MonteCarlo(
+        sources=diffuse_sources,
+        emin = emin,
+        emax = emax,
+        irf='P7SOURCE_V6',
+        roi_dir=roi_dir,
+        maxROI=10,
+        seed=0,
+        ft1=ft1,
+        ft2=ft2,
+        gtifile = ltcube,
+        savedir=savedir)
+    m.simulate()
