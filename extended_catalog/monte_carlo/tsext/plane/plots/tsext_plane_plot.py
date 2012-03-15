5a073479f89f98e48279513d91c979930fe3a2bb
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2012-03-15 16:55:38 +0000

    new plotting code

--- extended_catalog/monte_carlo/tsext/plane/plots/tsext_plane_plot.py
+++ extended_catalog/monte_carlo/tsext/plane/plots/tsext_plane_plot.py
@@ -1,6 +1,9 @@
+from os.path import join, expandvars
+
+import pylab as P
 import yaml
 import numpy as np
-from os.path import join, expandvars
+from scipy.stats import chi2
 
 from uw.utilities.makerec import fitsrec
 
@@ -16,7 +19,17 @@ ts_ext_list = r['ts_ext']
 
 ts_ext_list = np.where(ts_ext_list > 0, ts_ext_list, 0)
 
-for index in [1.5, 2, 2.5, 3]:
+max_ts_ext = max(ts_ext_list)
+
+fig = P.figure(None, figsize=(6,6))
+axes = fig.add_subplot(111)
+
+
+for index,kwargs in [[1.5,dict(color='blue', label='index=1.5')],
+                     [2,  dict(color='purple', label='index=2')],
+                     [2.5,dict(color='green', label='index=2.5')],
+                     [3,  dict(color='black', label='index=3')]
+                    ]:
 
     cut = close(index,index_list)
 
@@ -26,4 +39,23 @@ for index in [1.5, 2, 2.5, 3]:
     print '.. average ts_point',np.average(ts_point), np.std(ts_point)
     ts_ext = ts_ext_list[cut]
     print '.. average ts_ext',np.average(ts_ext), np.std(ts_ext)
-    print 
+
+
+    bins=np.linspace(0,max_ts_ext,1e3)
+    data=np.histogram(ts_ext,bins=bins)[0]
+    
+    cdf=np.cumsum(data[::-1])[::-1]
+    cdf=cdf.astype(float)/cdf[0] # normalize
+    cdf = np.append(cdf, 0)
+
+    axes.semilogy(bins,cdf,linewidth=1, **kwargs)
+
+
+lower,upper=axes.get_xlim()
+bins=np.linspace(lower,upper,10000)
+y = chi2.sf(bins,1)/2
+axes.semilogy(bins, y, color='red', linewidth=1, label='$\chi^2_1/2$', zorder=0, dashes=(5,3))
+
+P.legend()
+
+P.savefig('ts_ext_plane.pdf')
