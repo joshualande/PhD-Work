196fbb54763f8c5e3fad7b23721a4384624ec9be
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2012-03-22 22:31:12 +0000

    udpated

--- fermi/monte_carlo/simps/code/loop.py
+++ fermi/monte_carlo/simps/code/loop.py
@@ -1,7 +1,42 @@
-from lande.utilities.jobbuilder import JobBuilder
+from lande.utilities.simtools import SimBuilder
+from collections import OrderedDict
 
-b = JobBuilder(
-    savedir='$simpsdata/v1',
+#params=OrderedDict()
+#params['time']=['1day']
+#params['flux']=[1e-2, 1e-3]
+#params['position']=['allsky']
+#params['emin']=[1e2]
+#params['emax']=[1e5]
+#
+#b = SimBuilder(
+#    savedir='$simpsdata/v1',
+#    code='$simpscode/simulate.py',
+#    num=100,
+#    params=params)
+#b.build()
+
+
+# v2
+#params=OrderedDict()
+##params[('time','flux')]=[('1day',1e-2), ('2years',1e-4), ('2fgl',1e-4)]
+#params['time']=['1day']
+#params['flux']=[1e-2]
+#params['position']=['allsky']
+#params['emin']=[1e2]
+#params['emax']=[1e5]
+#
+#b = SimBuilder(
+#    savedir='$simpsdata/v2',
+#    code='$simpscode/simulate.py',
+#    num=100,
+#    params=params)
+#b.build()
+
+
+# v3
+b = SimBuilder(
+    savedir='$simpsdata/v3',
     code='$simpscode/simulate.py',
-    num=10)
+    num=100,
+    params=dict(time='1day', flux=1e-2, position='allsky', 1e2, 1e5))
 b.build()
--- fermi/monte_carlo/simps/code/simulate.py
+++ fermi/monte_carlo/simps/code/simulate.py
@@ -4,15 +4,15 @@ from lande.fermi.likelihood.roi_gtlike import Gtlike
 from argparse import ArgumentParser
 import shutil
 from tempfile import mkdtemp
-from os.path import join
+from os.path import join, exists
 
 import yaml
 import numpy as np
 
 from skymaps import SkyDir
 
-from uw.like.roi_monte_carlo import SpectralAnalysisMC
-from uw.like.pointspec import DataSpecification
+from uw.like.roi_monte_carlo import MonteCarlo
+from uw.like.pointspec import DataSpecification, SpectralAnalysis
 from uw.like.pointspec_helpers import PointSource
 from uw.like.Models import PowerLaw
 from uw.like.roi_state import PointlikeState
@@ -20,12 +20,19 @@ from uw.like.roi_state import PointlikeState
 from lande.fermi.likelihood.diffuse import get_sreekumar
 from lande.fermi.likelihood.save import sourcedict
 from lande.fermi.data.catalogs import dict2fgl
+from lande.fermi.data.livetime import gtltcube
 from lande.utilities.random import random_on_sphere
 from lande.utilities.save import savedict
 
 
 parser = ArgumentParser()
 parser.add_argument("i", type=int)
+parser.add_argument("--time", required=True, choices=['2fgl', '1day', '2years'])
+parser.add_argument("--flux", required=True, type=float)
+parser.add_argument("--position", required=True, choices=['galcenter', 'allsky' ])
+parser.add_argument("--emin", required=True, type=float)
+parser.add_argument("--emax", required=True, type=float)
+
 args= parser.parse_args()
 
 i=args.i
@@ -33,46 +40,97 @@ istr='%05d' % i
 
 np.random.seed(i)
 
-roi_dir = SkyDir(0,0,SkyDir.GALACTIC)
 name='source'
 
-emin=1e2
-emax=1e5
+flux=args.flux
+position=args.position
+time=args.time
+emin=args.emin
+emax=args.emax
+
+if position == 'galcenter':
+    roi_dir = SkyDir(0,0,SkyDir.GALACTIC)
+elif position == 'allsky':
+    roi_dir=random_on_sphere()
 
 model_mc = PowerLaw(index=2)
-model_mc.set_flux(1e-5, emin=emin, emax=emax)
+model_mc.set_flux(flux, emin=emin, emax=emax)
 
-sreekumar = get_sreekumar()
 ps = PointSource(name=name, model=model_mc, skydir=roi_dir)
+sreekumar = get_sreekumar()
 
-tempdir='savedir'
+tempdir=mkdtemp(prefix='/scratch/')
+
+if time == '1day':
+    ft2 = join(tempdir,'ft2.fits')
+    ltcube = join(tempdir,'ltcube.fits')
+    mc_kwargs=dict(
+        tstart=0,
+        tstop=86400,
+        ltfrac=0.9)
+elif time == '2years':
+    ft2 = join(tempdir,'ft2.fits')
+    ltcube = join(tempdir,'ltcube.fits')
+    mc_kwargs=dict(
+        tstart=0,
+        tstop=63113851.9,
+        ltfrac=0.9)
+elif time == '2fgl':
+    ft2 = dict2fgl['ft2']
+    ltcube = dict2fgl['ltcube']
+    mc_kwargs=dict(gtifile=ltcube)
+
+irf = 'P7SOURCE_V6'
+roi_size = 10*np.sqrt(2)
+ft1 = join(tempdir,'ft1.fits')
+
+if not exists(ft1):
+    mc = MonteCarlo(
+        ft1=ft1,
+        ft2=ft2,
+        sources=[ps,sreekumar],
+        emin=emin,
+        emax=emax,
+        irf=irf,
+        maxROI=roi_size,
+        savedir=tempdir,
+        seed=i,
+        **mc_kwargs)
+    mc.simulate()
+
+if not exists(ltcube):
+    gtltcube(evfile=ft1, scfile=ft2, outfile=ltcube, dcostheta=0.025, binsz=1)
 
 ds = DataSpecification(
-    ft1files = join(tempdir,'ft1.fits'),
-    ft2files = dict2fgl['ft2'],
-    ltcube = dict2fgl['ltcube'],
+    ft1files = ft1,
+    ft2files = ft2,
+    ltcube = ltcube,
     binfile = join(tempdir,'binned.fits'))
 
-sa = SpectralAnalysisMC(ds,
-                        binsperdec=8,
-                        emin = emin,
-                        emax = emax,
-                        irf='P7SOURCE_V6',
-                        roi_dir=roi_dir,
-                        minROI=10,
-                        maxROI=10,
-                        seed=i,
-                        mc_energy=True,
-                        savedir=tempdir,
-                       )
+sa = SpectralAnalysis(ds,
+                      binsperdec=8,
+                      emin = emin,
+                      emax = emax,
+                      irf=irf,
+                      event_class=0,
+                      roi_dir=roi_dir,
+                      minROI=roi_size,
+                      maxROI=roi_size,
+                      use_weighted_livetime=True,
+                     )
 
 roi = sa.roi(roi_dir=roi_dir, 
              point_sources=[ps],
-             diffuse_sources=[sreekumar])
+             diffuse_sources=[sreekumar],
+            )
 state = PointlikeState(roi)
 
 results = dict(
     i = i,
+    position = position,
+    time = time,
+    emin = emin,
+    emax = emax,
     istr = istr)
 
 mc=sourcedict(roi, name)
@@ -87,7 +145,8 @@ results['pointlike'] = dict(mc=mc, fit=fit)
 
 state.restore()
 
-gtlike = Gtlike(roi, bigger_roi=True)
+gtlike = Gtlike(roi, binsz=1/8., bigger_roi=False, 
+                enable_edisp=True, fix_pointlike_ltcube=True)
 like = gtlike.like
 
 mc=sourcedict(like, name)
@@ -99,3 +158,5 @@ fit = sourcedict(like, name)
 results['gtlike'] = dict(mc=mc, fit=fit)
 
 savedict('results_%s.yaml' % istr, results)
+
+shutil.rmtree(tempdir)
