c53f19b0512bd9bdfe46781662e38a1b035607cb
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2012-03-25 04:28:46 +0000

    plots

--- extended_catalog/monte_carlo/w44sim/plots/plot_bias.py
+++ extended_catalog/monte_carlo/w44sim/plots/plot_bias.py
@@ -1,29 +1,67 @@
 
 from os.path import join, expandvars
-
-from lande.utilities.pubplot import set_latex_defaults,get_bw,save
+import matplotlib.font_manager
+from matplotlib.ticker import MaxNLocator
 
 import numpy as np
 import h5py
 import pylab as P
 
+from lande.utilities.pubplot import set_latex_defaults,get_bw,save
+
 set_latex_defaults()
 bw=get_bw()
 
-savedir = expandvars(join('$w44simdata/','v4', 'merged.hdf5'))
+savedir = expandvars(join('$w44simdata/','v7', 'merged.hdf5'))
 results = h5py.File(savedir, 'r')
 
-r68_Disk = np.asarray(results['r68_Disk'])
-r68_Gaussian = np.asarray(results['r68_Gaussian'])
-r68_EllipticalDisk = np.asarray(results['r68_EllipticalDisk'])
-r68_EllipticalRing = np.asarray(results['r68_EllipticalRing'])
+def plot(axes, quantity, hist_kwargs):
 
-fig=P.figure(None, figsize=(6,6))
+    for type in ['Point', 'Disk', 'Gaussian', 'EllipticalDisk', 'EllipticalRing']:
 
-axes=fig.add_subplot(221)
-axes.hist(ts_point, **hist_kwargs)
-axes.xaxis.set_major_locator(MaxNLocator(4))
-axes.set_xlabel(r'$\textrm{TS}_\textrm{point}$')
+        name = '%s_%s' % (quantity,type)
+        if name in results:
+            array = np.asarray(results[name])
 
-save('w44sim')
+            print '%s, %s, average=%s' % (quantity, type, np.average(array))
+
+            # TEMPORARY
+            hist_kwargs['bins']=30
+            # TEMPORARY
+
+            axes.hist(array, label=type, **hist_kwargs)
+
+    mc = '%s_mc' % quantity
+    if mc in results:
+        r=results[mc]
+        r=r[0]
+        print '%s, mc=%s' % (quantity, r)
+        axes.axvline(r, label='mc')
+
+    # TEMPORARY
+    axes.set_ylim(ymax=axes.get_ylim()[1] + 300)
+    # TEMPORARY
+
+    axes.xaxis.set_major_locator(MaxNLocator(4))
+    prop = matplotlib.font_manager.FontProperties(size=10)
+    axes.legend(prop=prop)
+
+fig=P.figure(None, figsize=(7,3))
+
+hist_kwargs=dict(histtype='step')
+
+axes=fig.add_subplot(131)
+plot(axes,'r68',hist_kwargs=hist_kwargs)
+axes.set_xlabel('r68 (degrees)')
+
+axes=fig.add_subplot(132)
+plot(axes, 'flux', hist_kwargs=hist_kwargs)
+axes.set_xlabel('flux')
+
+axes=fig.add_subplot(133)
+plot(axes, 'index', hist_kwargs=hist_kwargs)
+axes.set_xlabel('Spectral Index')
+
+fig.subplots_adjust(bottom=0.2)
+
 save('bias_w44sim')
--- extended_catalog/monte_carlo/w44sim/plots/plot_ts_values.py
+++ extended_catalog/monte_carlo/w44sim/plots/plot_ts_values.py
@@ -1,18 +1,18 @@
 from os.path import join, expandvars
 
-from lande.utilities.pubplot import set_latex_defaults,get_bw,save
-
 import matplotlib.font_manager 
 from matplotlib.ticker import MaxNLocator
 import numpy as np
 import h5py
 import pylab as P
 
+from lande.utilities.pubplot import set_latex_defaults,get_bw,save
+
 set_latex_defaults()
 bw=get_bw()
 
-savedir = expandvars(join('$w44simdata/','v4', 'merged.hdf5'))
-results = h5py.File(savedir, 'r')
+merged = expandvars(join('$w44simdata/','v7', 'merged.hdf5'))
+results = h5py.File(merged, 'r')
 
 ts_point = np.asarray(results['TS_Point'])
 
@@ -22,34 +22,45 @@ ll_disk = np.asarray(results['ll_Disk'])
 ll_elliptical_disk = np.asarray(results['ll_EllipticalDisk'])
 ll_elliptical_ring = np.asarray(results['ll_EllipticalRing'])
 
+def histogram(axes, data, **kwargs):
+    #default_kwargs=dict(bins=30, histtype='step')
+    default_kwargs=dict(bins=30)
+    default_kwargs.update(kwargs)
+
+    axes.set_autoscaley_on(True)
+    axes.set_autoscaley_on(True)
+
+    bins = np.linspace(data.min(), data.max(), 30)
+    data = np.histogram(data, bins=bins)
+
+    axes.plot(bins, data)
+    #axes.hist(data, **default_kwargs)
+
+    axes.xaxis.set_major_locator(MaxNLocator(4))
+
+
 fig=P.figure(None, figsize=(6,6))
 
-#hist_kwargs=dict(histtype='step')
-hist_kwargs=dict()
 
 axes=fig.add_subplot(221)
-axes.hist(ts_point, **hist_kwargs)
-axes.xaxis.set_major_locator(MaxNLocator(4))
+histogram(axes, ts_point)
 axes.set_xlabel(r'$\textrm{TS}_\textrm{point}$')
 
 axes=fig.add_subplot(222)
-axes.hist(2*(ll_disk-ll_point), label='Disk', **hist_kwargs)
-axes.hist(2*(ll_gaussian-ll_point), label='Gaussian', **hist_kwargs)
-axes.xaxis.set_major_locator(MaxNLocator(4))
+histogram(axes, 2*(ll_disk-ll_point), label='Disk')
+histogram(axes, 2*(ll_gaussian-ll_point), label='Gaussian')
 axes.set_xlabel(r'$\textrm{TS}_\textrm{ext}$')
-
 prop = matplotlib.font_manager.FontProperties(size=10)
 axes.legend(prop=prop)
 
 axes=fig.add_subplot(223)
-axes.hist(2*(ll_elliptical_disk-ll_disk), **hist_kwargs)
+histogram(axes, 2*(ll_elliptical_disk-ll_disk))
 axes.set_xlabel(r'$\textrm{TS}_\textrm{ellip. disk}-\textrm{TS}_\textrm{disk}$')
 
 axes=fig.add_subplot(224)
-
 ts_inc = 2*(ll_elliptical_ring-ll_elliptical_disk)
 ts_inc = np.where(ts_inc > 0, ts_inc, 0)
-axes.hist(ts_inc, **hist_kwargs)
+histogram(axes, ts_inc)
 axes.set_xlabel(r'$\textrm{TS}_\textrm{ellip. ring}-\textrm{TS}_\textrm{ellip. disk}$')
 
 save('ts_comparision_w44sim')
