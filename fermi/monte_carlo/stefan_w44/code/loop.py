b2581393b924711d2d4b65ea93c8f2aacefe1fa3
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2012-03-28 20:54:52 +0000

    updating

--- fermi/monte_carlo/stefan_w44/code/loop.py
+++ fermi/monte_carlo/stefan_w44/code/loop.py
@@ -1,6 +1,6 @@
 from lande.utilities.simtools import SimBuilder
 
-params=dict(spectrum=[ 'SmoothBrokenPowerLaw', 'DoubleBrokenSmoothPowerlaw'])
+params=dict(spectrum=[ 'SmoothBrokenPowerLaw', 'SmoothDoubleBrokenPowerLaw'])
 j = SimBuilder(savedir='$stefan_w44_data/v1',
                code='$stefan_w44_code/simulate.py',
                num=10,
--- fermi/monte_carlo/stefan_w44/code/simulate.py
+++ fermi/monte_carlo/stefan_w44/code/simulate.py
@@ -6,7 +6,7 @@ from argparse import ArgumentParser
 import numpy as np
 from skymaps import SkyDir
 from uw.utilities.xml_parsers import parse_sources
-from uw.like.Models import FileFunction, ProductModel, Constant
+from uw.like.Models import SmoothBrokenPowerLaw, SmoothDoubleBrokenPowerLaw
 from uw.like.roi_catalogs import Catalog2FGL
 from uw.like.pointspec_helpers import get_default_diffuse
 from uw.like.roi_monte_carlo import MonteCarlo
@@ -14,7 +14,7 @@ from uw.like.roi_monte_carlo import MonteCarlo
 
 parser = ArgumentParser()
 parser.add_argument("i", type=int)
-parser.add_argument("--spectrum", required=True, choices=['SmoothBrokenPowerLaw', 'DoubleBrokenSmoothPowerlaw'])
+parser.add_argument("--spectrum", required=True, choices=['SmoothBrokenPowerLaw', 'SmoothDoubleBrokenPowerLaw'])
 args= parser.parse_args()
 
 spectrum = args.spectrum
@@ -29,9 +29,8 @@ ltcube = '/u/gl/funk/data/GLAST/ExtendedSources/NewAnalysis/gtlike/W44/ltcube_23
 ft1 = 'simulated_ft1.fits'
 
 ps,ds=parse_sources('/u/gl/funk/data/GLAST/ExtendedSources/NewAnalysis/gtlike/W44/ExploreSources/W44_fitted_BINNED_freeClose.PlusP.xml')
-sources = ps + ds
 
-d = {s.name:s for s in sources}
+d = {s.name:s for s in ds}
 
 # Fix diffuse components
 iso = d['iso_p7v6source'] 
@@ -42,23 +41,46 @@ gal.smodel['Index'] = 1
 
 w44 = d['2FGL J1855.9+0121e']
 
+catalog = Catalog2FGL('$FERMI/catalogs/gll_psc_v05.fit',
+                      latextdir='$FERMI/extended_archives/gll_psc_v05_templates')
+catalog_w44 = catalog.get_source('W44')
+
 print 'Before flux for w44',w44.model.i_flux(1e2,1e5)
 
-if spectrum == 'LogParabola':
-    # w44 in xml is already log parabola
-    pass
-elif spectrum == 'FileFunction':
-    file = join(expandvars('$stefan_w44_code'), 'W44_pi0_model_gtobssim.dat')
-    w44.model = ProductModel(Constant(scale=1e-4),FileFunction(file=file))
+if spectrum == 'SmoothBrokenPowerLaw':
+    w44.model = SmoothDoubleBrokenPowerLaw(
+        scale=2e8,
+        index1=1.711,
+        index=1.711,
+        breakvalue12=217,
+        beta12=0.03,
+        index3=2.85,
+        breakvalue23=1723,
+        beta23=0.2,
+    )
+elif spectrum == 'SmoothDoubleBrokenPowerLaw':
+    w44.model = SmoothDoubleBrokenPowerLaw(
+        scale=2e8,
+        index1=0.024,
+        index=1.711,
+        breakvalue12=217,
+        beta12=0.03,
+        index3=2.85,
+        breakvalue23=1723,
+        beta23=0.2,
+    )
+    w44.model = SmoothDoubleBrokenPowerLaw()
 
 else:
     raise Exception("...")
+
+w44.model.set_flux(catalog_w44.model.i_flux(emin=100,emax=1e5),emin=100,emax=1e5)
 
 print 'After flux used to simulate w44',w44.model.i_flux(1e2,1e5)
 
 irf='P7SOURCE_V6'
 mc=MonteCarlo(
-    sources=sources,
+    sources=ps+ds,
     seed=i,
     irf=irf,
     ft1=ft1,
@@ -67,9 +89,11 @@ mc=MonteCarlo(
     maxROI=15,
     emin=emin,
     emax=emax,
+    energy_pad=1,
     gtifile=ltcube,
     savedir='gtobssim_output')
 mc.simulate()
 
 
+write_sources(ps,ds,'gtlike_model.xml')
 
