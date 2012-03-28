42d3566c75f049257b02cae1211c63ff7c553697
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2012-03-28 18:18:15 +0000

    update

--- fermi/monte_carlo/stefan_w44/code/loop.py
+++ fermi/monte_carlo/stefan_w44/code/loop.py
@@ -1,21 +1,9 @@
-from os.path import join, expandvars, exists
-from os import makedirs
+from lande.utilities.simtools import SimBuilder
 
-num=5
-savedir = expandvars('$stefan_w44_data/v1')
+params=dict(spectrum=[ 'SmoothBrokenPowerLaw', 'DoubleBrokenSmoothPowerlaw'])
+j = SimBuilder(savedir='$stefan_w44_data/v1',
+               code='$stefan_w44_code/simulate.py',
+               num=10,
+               params=params)
+j.build()
 
-for model in ['FileFunction', 'LogParabola']:
-
-    subdir = join(savedir, model)
-
-    for i in range(num):
-        istr='%05d' % i
-
-        jobdir = join(subdir,istr)
-        if not exists(jobdir): makedirs(jobdir)
-
-        run = join(jobdir,'run.sh')
-        open(run,'w').write("""python $stefan_w44_code/simulate.py %g --spectrum %s""" % (i,model))
-
-submit_all = join(savedir,'submit_all.sh')
-open(submit_all,'w').write("submit_all */*/run.sh $@")
--- fermi/monte_carlo/stefan_w44/code/simulate.py
+++ fermi/monte_carlo/stefan_w44/code/simulate.py
@@ -14,15 +14,15 @@ from uw.like.roi_monte_carlo import MonteCarlo
 
 parser = ArgumentParser()
 parser.add_argument("i", type=int)
-parser.add_argument("--spectrum", required=True, choices=['LogParabola', 'FileFunction'])
+parser.add_argument("--spectrum", required=True, choices=['SmoothBrokenPowerLaw', 'DoubleBrokenSmoothPowerlaw'])
 args= parser.parse_args()
 
 spectrum = args.spectrum
 i = args.i
 istr = '%05d' % i
 
-emin=1e2
-emax=1e5
+emin=10
+emax=1e6
 
 ft2='/u/gl/bechtol/disk/drafts/radio_quiet/36m_gtlike/trial_v1/ft2-30s_239557414_334152027.fits'
 ltcube = '/u/gl/funk/data/GLAST/ExtendedSources/NewAnalysis/gtlike/W44/ltcube_239557414_334152002.fits'
@@ -40,7 +40,6 @@ gal = d['gal_2yearp7v6_v0']
 gal.smodel['Norm'] = 1
 gal.smodel['Index'] = 1
 
-
 w44 = d['2FGL J1855.9+0121e']
 
 print 'Before flux for w44',w44.model.i_flux(1e2,1e5)
@@ -49,7 +48,6 @@ if spectrum == 'LogParabola':
     # w44 in xml is already log parabola
     pass
 elif spectrum == 'FileFunction':
-
     file = join(expandvars('$stefan_w44_code'), 'W44_pi0_model_gtobssim.dat')
     w44.model = ProductModel(Constant(scale=1e-4),FileFunction(file=file))
 
