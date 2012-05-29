32817b9c656a0c453c6e4e025299ac8209bcef30
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2012-05-29 22:27:20 +0000

    update

---
+++ pwncatalog/PWNCAT2/code/lande/off_peak/loop.py
@@ -0,0 +1,16 @@
+import yaml
+from os.path import expandvars
+from lande.utilities.jobtools import JobBuilder
+
+pwndata="$pwnpersonal/data_prep/v2/pwncat2_data_lande.yaml"
+names=yaml.load(open(expandvars(pwndata))).keys()
+params=dict(pwncat1phase="$pwncode/pwndata/pwncat1_phase.yaml",
+            pwndata=pwndata,
+            name=names)
+
+b = JobBuilder(
+    savedir='$pwndata/off_peak/off_peak_bb/pwncat2/v5/analysis',
+    code='$pwncode/lande/off_peak/off_peak_bb.py',
+    params=params,
+    short_folder_names=True)
+b.build()
