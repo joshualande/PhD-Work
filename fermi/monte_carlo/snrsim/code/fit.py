43651dd62f28cfe7ddafdea78a952f82bad28707
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2012-06-29 21:31:26 +0000

    update

--- fermi/monte_carlo/snrsim/code/fit.py
+++ fermi/monte_carlo/snrsim/code/fit.py
@@ -1,10 +1,16 @@
+from lande.fermi.likelihood.roi_gtlike import Gtlike
+
 from argparse import ArgumentParser
 from os.path import join, expandvars
 
-from skymaps import SkyDir  
-from uw.like.pointspec import DataSpecification, SpectralAnalysis  
 
-from lande.fermi.likelihood.roi_gtlike import Gtlike
+from skymaps import SkyDir  
+
+from uw.like.pointspec import DataSpecification, SpectralAnalysis  
+from uw.like.roi_state import PointlikeState
+
+from lande.fermi.likelihood.save import sourcedict
+from lande.utilities.save import savedict
 from lande.fermi.sed.supersed import SuperSED
 from lande.fermi.sed.pointlike import pointlike_sed_to_yaml
 
@@ -60,25 +66,42 @@ roi = sa.roi_from_xml(
     fit_emax=10**3.25, # 1778 MeV
 )
 
+state=PointlikeState(roi)
+
+results=dict()
+
 print 'bins',roi.bin_edges
 
 roi.print_summary(galactic=True)
 
-"""
-roi.fit(use_gradient=False)
+results['pointlike'] = dict()
+results['pointlike']['mc'] = sourcedict(roi,which,errors=False)
+
+roi.fit(use_gradient=False, fit_bg_first = True)
+results['pointlike']['fit'] = sourcedict(roi,which)
 
 roi.print_summary(galactic=True)
-"""
+print roi
 
 roi.plot_counts_map(filename='counts_map.pdf')
 
+roi.print_summary(galactic=True)
+print roi
 
 s=roi.plot_sed(which=which,filename='sed_pointlike.pdf', use_ergs=True)
 pointlike_sed_to_yaml(s,'sed_pointlike.yaml')
+roi.toXML('results_pointlike.xml')
 
+state.restore()
 
-gtlike = Gtlike(roi, savedir='gtlike_savedir')
+gtlike = Gtlike(roi, savedir='gtlike_savedir', enable_edisp=True)
 like = gtlike.like
+
+results['gtlike'] = dict()
+results['gtlike']['mc'] = sourcedict(like,name,errors=False)
+
+like.fit(covar=True)
+results['gtlike']['fit'] = sourcedict(like,name)
 
 if args.debug: 
     # in debug mode, there is no background model,
@@ -91,3 +114,6 @@ else:
 sed = SuperSED(like, name=name, **sed_kwargs)
 sed.save('sed_gtlike.yaml')
 sed.plot('sed_gtlike.pdf')
+
+savedict('results_%s.yaml' % name,results)
+like.writeXml('results_gtlike.xml')
--- fermi/monte_carlo/snrsim/code/loop.py
+++ fermi/monte_carlo/snrsim/code/loop.py
@@ -41,17 +41,58 @@ from lande.utilities.jobtools import JobBuilder
 #j.build()
 
 
+
+#params=dict(source=['W44','IC443'], 
+#            simdir=join("$snrsim_sims","v3"))
+#params['spectrum','normalization']=[['PowerLaw','same_prefactor'],
+#                                    ['PowerLaw','same_flux'],
+#                                    ['SmoothBrokenPowerLaw','standard'],
+#                                   ]
+#
+#j = JobBuilder(savedir='$snrsim_fits/v1',
+#               code='$snrsim_code/fit.py',
+#               num=5,
+#               params=params)
+#j.build()
+
+#params=dict(source=['W44','IC443'], 
+#            simdir=join("$snrsim_sims","v3"))
+#params['spectrum','normalization']=[['PowerLaw','same_prefactor'],
+#                                    ['PowerLaw','same_flux'],
+#                                    ['SmoothBrokenPowerLaw','standard'],
+#                                   ]
+#
+#j = JobBuilder(savedir='$snrsim_fits/v2',
+#               code='$snrsim_code/fit.py',
+#               num=5,
+#               params=params)
+#j.build()
+
+
+
+
+#params=dict(source=['W44','IC443'], 
+#            simdir=join("$snrsim_sims","v3"))
+#params['spectrum','normalization']=[['PowerLaw','same_prefactor'],
+#                                    ['PowerLaw','same_flux'],
+#                                    ['SmoothBrokenPowerLaw','standard'],
+#                                   ]
+#
+#j = JobBuilder(savedir='$snrsim_fits/v3',
+#               code='$snrsim_code/fit.py',
+#               num=5,
+#               params=params)
+#j.build()
+
 params=dict(source=['W44','IC443'], 
             simdir=join("$snrsim_sims","v3"))
-
 params['spectrum','normalization']=[['PowerLaw','same_prefactor'],
                                     ['PowerLaw','same_flux'],
                                     ['SmoothBrokenPowerLaw','standard'],
                                    ]
 
-j = JobBuilder(savedir='$snrsim_fits/v1',
+j = JobBuilder(savedir='$snrsim_fits/v4',
                code='$snrsim_code/fit.py',
                num=5,
                params=params)
 j.build()
-
