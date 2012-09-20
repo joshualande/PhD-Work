365861038b6feaea7d3f4a31e8aeb44419ba8964
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2012-09-20 16:55:37 +0000

    udpate

--- fermi/pwn/pwncat2/classify/classify.py
+++ fermi/pwn/pwncat2/classify/classify.py
@@ -1,16 +1,16 @@
 from lande.fermi.pipeline.pwncat2.interp.classify import auto_classify, make_manual_classify
 from lande.utilities.save import savedict
 
-"""
 pwndata="$pwndata/pwncat2_data_lande.yaml"
 fitdir='$pwnpipeline/v35/analysis'
 c=auto_classify(pwndata, fitdir)
 savedict(c,'$pwnpipeline/v35/classify/automatic_classifications.yaml')
-"""
 
-#import sys;sys.exit(1)
+"""
+# don't run again
 c=make_manual_classify(
     pwndata="$pwndata/pwncat2_data_lande.yaml", 
-    fitdir='$pwnpipeline/v35/analysis'
-)
-savedict(c,'$pwnclassify/v35/manual_classifications.yaml')
+    fitdir='$pwnpipeline/v35/analysis')
+savedict(c,'$pwnclassify/manual_classifications.yaml')
+"""
+
