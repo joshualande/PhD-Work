9b7abf540ca7c176900bbb4d1e9f9f192c1417c1
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2012-09-21 19:10:05 +0000

    remove

--- fermi/pwn/pwncat2/tables/auxiliary_table.py
+++ fermi/pwn/pwncat2/tables/auxiliary_table.py
@@ -1,8 +1,18 @@
-from lande.pipeline.pwncat2.table.auxiliary import auxiliary_table
+from lande.fermi.pipeline.pwncat2.table.auxiliary import auxiliary_table
 
+"""
 auxiliary_table(
     pwndata='$pwncode/data/pwncat2_data_lande.yaml',
     fitdir='$pwndata/spectral/v29/analysis',
     filename='$pwndata/spectral/v29/tables/auxiliary_table.fits',
     pwn_classification='$pwndata/spectral/v29/classify/automatic_classifications.yaml',
+)
+"""
+
+auxiliary_table(
+    pwndata='$pwndata/pwncat2_data_lande.yaml',
+    pwnphase='$pwndata/pwncat2_phase_lande.yaml',
+    fitdir='$pwnpipeline/v35/analysis',
+    filename='$pwnpipeline/v35/tables/auxiliary_table.fits',
+    pwn_classification='$pwnclassify/manual_classifications.yaml'
 )
