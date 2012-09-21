6a663d08edeaee1214dd3567ca9cc6d27de60856
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2012-09-21 22:08:58 +0000

    update

--- fermi/pwn/pwncat2/classify/classify.py
+++ fermi/pwn/pwncat2/classify/classify.py
@@ -1,11 +1,12 @@
 from lande.fermi.pipeline.pwncat2.interp.classify import PWNAutomaticClassifier
 from lande.utilities.save import savedict
 
-pwndata="$pwndata/pwncat2_data_lande.yaml"
-fitdir='$pwnpipeline/v35/analysis'
-c=PWNAutomaticClassifier.get_automatic_classify(pwndata, fitdir)
-print c
-savedict(c,'$pwnpipeline/v35/classify/automatic_classifications.yaml')
+if True:
+    pwndata="$pwndata/pwncat2_data_lande.yaml"
+    fitdir='$pwnpipeline/v35/analysis'
+    c=PWNAutomaticClassifier.get_automatic_classify(pwndata, fitdir)
+    print c
+    savedict(c,'$pwnpipeline/v35/classify/automatic_classifications.yaml')
 
 
 if False:
