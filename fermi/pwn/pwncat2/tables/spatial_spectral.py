57cf8d7d91748e4dd2006c826361be77a09e8d32
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2012-09-24 07:17:09 +0000

    update

--- fermi/pwn/pwncat2/tables/spatial_spectral.py
+++ fermi/pwn/pwncat2/tables/spatial_spectral.py
@@ -1,13 +1,14 @@
-from lande.pipeline.pwncat2.table.spatial_spectral import spatial_spectral_table
+from lande.fermi.pipeline.pwncat2.table.spatial_spectral import spatial_spectral_table
 
 from lande.utilities.table import get_table_type
 table_type=get_table_type()
 
 spatial_spectral_table(
-    pwndata='$pwncode/data/pwncat2_data_lande.yaml',
-    fitdir='$pwndata/spectral/v29/analysis',
-    savedir='$pwndata/spectral/v29/tables',
+    pwndata='$pwndata/pwncat2_data_lande.yaml',
+    pwnphase='$pwndata/pwncat2_phase_lande.yaml',
+    fitdir='$pwnpipeline/v35/analysis',
+    savedir='$pwnpipeline/v35/tables',
     filebase='off_peak_spatial_spectral',
     table_type=table_type,
-    pwn_classification='$pwndata/spectral/v29/classify/automatic_classifications.yaml',
+    pwn_classification='$pwnclassify/manual_classifications.yaml',
 )
