ce4d4655adef16675a982c3a6b973074bd9559ae
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2012-10-31 00:29:03 +0000

    update

---
+++ fermi/gamma_quiet_psrs/pipeline/followup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from argparse import ArgumentParser
+
+from lande.utilities.argumentparsing import parse_strip_known_args
+
+from lande.fermi.pipeline.gamma_quiet_psrs.analysis.pipeline import Pipeline
+
+parser = ArgumentParser()
+parser.add_argument("--hypothesis", required=True, choices=['at_pulsar', 'point', 'extended'])
+parser.add_argument("--followup", required=True, choices=['tsmaps','plots', 'gtlike'])
+followup_args = parse_strip_known_args(parser)
+hypothesis = followup_args.hypothesis
+followup =followup_args.followup
+
+kwargs = Pipeline.get_kwargs()
+
+pipeline=Pipeline(**kwargs)
+
+if followup == 'gtlike':
+    pipeline.gtlike_followup(hypothesis=hypothesis)
+elif followup == 'variability':
+    pipeline.variability_followup(hypothesis=hypothesis)
+elif followup == 'tsmaps':
+    pipeline.tsmaps_followup(hypothesis=hypothesis)
+elif followup == 'plots':
+    pipeline.plots_followup(hypothesis=hypothesis)
--- fermi/gamma_quiet_psrs/pipeline/loop.py
+++ fermi/gamma_quiet_psrs/pipeline/loop.py
@@ -3,34 +3,8 @@ from textwrap import dedent
 from itertools import product
 
 from lande.utilities.save import loaddict
-from lande.utilities.jobtools import JobBuilder
 
-
-class PipelineBuilder(JobBuilder):
-    def build_followup(self, code, hypotheses, followups):
-        for perm in product(*self.values):
-            subdir = self.build_subdir(perm)
-            args = self.build_args(perm)
-
-            for hypothesis in hypotheses:
-                for followup in followups:
-                    run = join(subdir,'followup_%s_%s.sh' % (hypothesis,followup))
-                    open(run,'w').write("python %s --hypothesis=%s --followup=%s %s" % (code,args,hypothesis,followup))
-
-
-            followup_all = join(self.savedir,'followup_all.sh')
-            open(followup_all,'w').write(dedent("""
-            #!/usr/bin/env bash
-            submit_list=""
-            for pwn in PSR*; do
-                for hypothesis in at_pulsar point extended; do
-                    if [ -e $pwn/roi_${hypothesis}_${pwn}.dat -a -e $pwn/results_${pwn}_pointlike_${hypothesis}.yaml ]; then
-                        submit_list="$pwn/followup_${pwn}_*_${hypothesis}.sh $submit_list"
-                    fi
-                done
-            done
-            submit_all $submit_list $@
-            """))
+from lande.fermi.pipeline.pwncat2.analysis.builder import PipelineBuilder
 
 
 radiopsr_data='$gamma_quiet_psrs_data/gamma_quiet_psrs_data.yaml'
@@ -38,6 +12,7 @@ names=loaddict(radiopsr_data).keys()
 bigfile='$lat2pc/BigFile/Pulsars_BigFile_v20121002103223.fits'
 
 params=dict(name=names, 
+            fast=True,
             bigfile=bigfile)
 params['radiopsr-data']=radiopsr_data
 b = PipelineBuilder(
@@ -50,4 +25,5 @@ b.build()
 b.build_followup(
     code = '$gamma_quiet_psrs_pipeline/followup.py',
     hypotheses=['at_pulsar','point','extended'],
-    followups=['gtlike','plots','tsmaps'])
+    followups=['gtlike','plots','tsmaps']
+)
