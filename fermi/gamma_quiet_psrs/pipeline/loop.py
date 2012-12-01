d00985dae6066c89273c9f38c9493552a1bcd026
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2012-12-01 23:04:45 +0000

    update

--- fermi/gamma_quiet_psrs/pipeline/loop.py
+++ fermi/gamma_quiet_psrs/pipeline/loop.py
@@ -6,7 +6,9 @@ from lande.utilities.save import loaddict
 
 from lande.fermi.pipeline.pwncat2.analysis.builder import PipelineBuilder
 
+####################################################################################################
 
+"""
 radiopsr_data='$gamma_quiet_psrs_data/gamma_quiet_psrs_data.yaml'
 names=loaddict(radiopsr_data).keys()
 bigfile='$lat2pc/BigFile/Pulsars_BigFile_v20121002103223.fits'
@@ -24,5 +26,30 @@ b.build()
 b.build_followup(
     code = '$gamma_quiet_psrs_pipeline/followup.py',
     hypotheses=['at_pulsar','point','extended'],
+    followups=['gtlike','plots','tsmaps']
+)
+"""
+
+####################################################################################################
+
+radiopsr_data='$gamma_quiet_psrs_data/gamma_quiet_psrs_data.yaml'
+names=loaddict(radiopsr_data).keys()
+bigfile='$lat2pc/BigFile/Pulsars_BigFile_v20121127171828.fits'
+
+params=dict(name=names, 
+            bigfile=bigfile)
+params['no-point']=True
+params['no-extended']=True
+params['radiopsr-data']=radiopsr_data
+b = PipelineBuilder(
+    savedir='$gamma_quiet_psrs_analysis/v2/fits',
+    code='$gamma_quiet_psrs_pipeline/main.py',
+    params=params,
+    short_folder_names=True,
+)
+b.build()
+b.build_followup(
+    code = '$gamma_quiet_psrs_pipeline/followup.py',
+    hypotheses=['at_pulsar'],
     followups=['gtlike','plots','tsmaps']
 )
--- fermi/gamma_quiet_psrs/website/website.py
+++ fermi/gamma_quiet_psrs/website/website.py
@@ -1,9 +1,19 @@
 from lande.fermi.pipeline.gamma_quiet_psrs.website.builder import WebsiteBuilder
 
+"""
 b=WebsiteBuilder(
     gamma_quiet_psrs_data='$gamma_quiet_psrs_data/gamma_quiet_psrs_data.yaml',
     analysisdir='$gamma_quiet_psrs_analysis/v1',
     webdir='$gamma_quiet_psrs_analysis/v1/website')
+b.build_main()
+b.build_all_psrs()
+"""
+
+
+b=WebsiteBuilder(
+    gamma_quiet_psrs_data='$gamma_quiet_psrs_data/gamma_quiet_psrs_data.yaml',
+    analysisdir='$gamma_quiet_psrs_analysis/v2',
+    webdir='$gamma_quiet_psrs_analysis/v2/website')
 #b.build_main()
 b.build_all_psrs()
 
