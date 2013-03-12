7a7613604e345c2eb567d9d1ec404095a229d778
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2013-03-12 23:25:31 +0000

    update

--- fermi/pwn/pwncat2/classify/compare.py
+++ fermi/pwn/pwncat2/classify/compare.py
@@ -1,6 +1,6 @@
 from lande.fermi.pipeline.pwncat2.interp.classify import compare_classifications
 
 pwndata="$pwndata/pwncat2_data_lande.yaml"
-fitdir='$pwnpipeline/v36/analysis'
+fitdir='$pwnpipeline/v37/analysis'
 pwn_classification='$pwnclassify/manual_classifications.yaml'
 compare_classifications(pwndata,fitdir,pwn_classification)
