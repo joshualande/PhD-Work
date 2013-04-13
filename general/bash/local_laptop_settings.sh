3f3cf5b2ee1c2a33e62d43ea5d0f720ec79b1566
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2013-04-13 17:33:26 +0000

    update

--- general/bash/local_laptop_settings.sh
+++ general/bash/local_laptop_settings.sh
@@ -1,12 +1,16 @@
 
-def ipyssh {
-        # usage: ipyssh lande@ki-ls03.slac.stanford.edu kernel-dd803c1b-65bc-468a-8e55-67655476a9f3.json
+function ipyssh {
+
+	if [ $# -ne 2 ]; then
+	  echo "Usage: ipyssh SSH_HOST KERNEL"
+	  echo "Example: ipyssh lande@ki-ls03.slac.stanford.edu kernel-dd803c1b-65bc-468a-8e55-67655476a9f3.json"
+	  return 0
+	fi
 
         host=$1
         kernel=$2
 
         tempfile=`mktemp /tmp/XXXXXXXXXX`
-        echo $tempfile
 
         ssh $host "cat ~/.config/ipython/profile_nbserver/security/$kernel" > $tempfile
 
