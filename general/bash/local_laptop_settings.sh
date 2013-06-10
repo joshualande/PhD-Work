a819f8f9456f6ee9ff5aa06c63b3bcb937a5fc79
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2013-06-10 21:44:47 +0000

    udpate

--- general/bash/local_laptop_settings.sh
+++ general/bash/local_laptop_settings.sh
@@ -38,7 +38,11 @@ function setup_coursera_ml {
     #export coursera_ml=/Users/joshualande/Google\ Drive/Career/resources/courses/Stanford\ Machine\ Learning/My\ Work/homework
     export coursera_ml=/Users/joshualande/Documents/work_svn/trunk/courses/coursera_ml/homework/
     export ps1=$coursera_ml/ps1/ex1
-    export ps2=$coursera_ml/ps2/ex2
+    export ps2=$coursera_ml/ps2/ex2-003/mlclass-ex2
+    export ps3=$coursera_ml/ps3/ex3-003/mlclass-ex3
+    export ps4=$coursera_ml/ps4/ex4-003/mlclass-ex4
+    export ps5=$coursera_ml/ps5/ex5-003/mlclass-ex5
+
     alias octave="exec '/Applications/Octave.app/Contents/Resources/bin/octave'"
 
 }
@@ -47,7 +51,16 @@ function setup_preferences {
 	set -o vi
 }
 
+function setup_insight {
+    export insight_project="/Users/joshualande/Google Drive/Career/Insight Data Science Fellows Program/project"
+    export reviewskim="/Users/joshualande/Documents/ReviewSkim/"
+    export PYTHONPATH=$reviewskim
+    export insight_imdb_db="$project/insight_imdb_db"
+}
+
+
 function setup_defaults {
+    setup_insight
 	setup_paths
 	setup_preferences
 	setup_coursera_ml
